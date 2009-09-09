0ee48107baa55bc9cc2111650178273f7be81934
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2009-09-09 10:52:40 +0000

    Update of testing scripts

--- trunk/test/RadialDoseDistributionTests.R
+++ trunk/test/RadialDoseDistributionTests.R
@@ -1,9 +1,14 @@
+# clear workspace
 rm( list = ls() )
-dyn.load("/home/grzanka/workspace/DKFZ/Steffen/SGParticleLibrary/Release/libSGPL.so")
-source("SGP.ssc")
-library("lattice")
 
-pdf("/home/grzanka/workspace/DKFZ/Steffen/SGParticleLibrary/SGPL-R/RDD.pdf")
+# load libAmTrack library
+dyn.load("../Release/libAmTrack.dll")
+
+# load wrapping scripts
+source("../WrappingScripts/SGP.ssc")
+
+# necessary library for plotting
+library("lattice")
 
 ####################### DOSE(DISTANCE) function test #########################################
 
@@ -64,10 +69,10 @@ for( i in ER.model) {
 # plots...
 
 p1 <- xyplot( D.Gy ~ r.m | ER.model.name , groups = RDD.model.name, ref = TRUE, data=df1, pch = ".", lty = 1, type = "l", xlab = "Distance [m]", ylab = "Dose [Gy]", auto.key = list(title = "Protons in liquid water, RDD",points = FALSE, lines = TRUE), scales = list(log = 10))
-#p2 <- xyplot( D.Gy ~ r.m | ER.model.name , groups = RDD.model.name, ref = TRUE, data=df1, pch = ".", lty = 1, type = "l", xlab = "Distance [m]", ylab = "Dose [Gy]", auto.key = list(title = "Protons in liquid water, RDD",points = FALSE, lines = TRUE))
+p2 <- xyplot( D.Gy ~ r.m | ER.model.name , groups = RDD.model.name, ref = TRUE, data=df1, pch = ".", lty = 1, type = "l", xlab = "Distance [m]", ylab = "Dose [Gy]", auto.key = list(title = "Protons in liquid water, RDD",points = FALSE, lines = TRUE))
 
 p1
-#p2
+p2
 
 ####################### DISTANCE(DOSE) function test #########################################
 
@@ -89,26 +94,31 @@ df2$r.m	<- numeric(nrow(df2))
 E.MeV.u = rep( 100, nrow(df2))
 particle.no = rep( 1, nrow(df2))
 
-df2
+#df2
 
-RDD.parameters
+#RDD.parameters
 
 for( i in ER.model) {
  ii 			<- df2$ER.model == i
 for( j in RDD.model) {
  	jj 			<- ((df2$RDD.model == j) & (df2$ER.model == i)) 
- 	cat("j = ",j," , parameters = ",RDD.parameters[[j]],"\n")
 		df2$r.m[jj] <- SGP.RDD.r.m(D.Gy = df2$D.Gy[jj], E.MeV.u, particle.no, material.no, ER.model = i, ER.parameters = ER.parameters, RDD.model = j, RDD.parameters=RDD.parameters[[j]] ) 	
 		}
 }
 
-df2
+#df2
 
 # plots...
 
 p3 <- xyplot( r.m ~ D.Gy | ER.model.name , groups = RDD.model.name, ref = TRUE, data=df2, pch = ".", lty = 1, type = "l", xlab = "Dose [Gy]", ylab = "Distance [m]", auto.key = list(title = "Protons in liquid water, inverse RDD",points = FALSE, lines = TRUE), scales = list(log = 10))
 p4 <- xyplot( r.m ~ D.Gy, groups = RDD.model.name, ref = TRUE, data=df2, pch = ".", lty = 1, type = "l", xlab = "Dose [Gy]", ylab = "Distance [m]", auto.key = list(title = "Protons in liquid water, inverse RDD",points = FALSE, lines = TRUE), scales = list(log = 10))
 
+# saving plots to the file
+
+pdf("RDD.pdf")
+
+p1
+p2
 p3
 p4
 
