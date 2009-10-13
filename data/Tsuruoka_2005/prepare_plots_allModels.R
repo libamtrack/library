1316d4f6d2b7451fb381282eb35d110b10369b74
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2009-10-13 13:30:34 +0000

    "copied"

---
+++ data/Tsuruoka_2005/prepare_plots_allModels.R
@@ -0,0 +1,123 @@
+# clear workspace
+rm( list = ls() )
+
+# load libAmTrack library
+dyn.load("../../Release/libAmTrack.dll")
+
+# load wrapping scripts
+source("../../WrappingScripts/SGP.ssc")
+
+library("gplots")
+library("lattice")
+
+############ read data from files #########################
+
+SGP.survival.data.read	<-	function(	){
+
+# read gamma data
+	SurvTable <- read.table("Tsuruoka_gamma.dat")
+ SurvTable$radiation = "gamma"
+	SurvTable$Z = 0
+	SurvTable$beta = 0
+	SurvTable$zzbb = 0
+	SurvTable$LET = 0
+	SurvTable$Init.Energy = 0
+	
+# read ion data
+	tmp.SurvTable <- read.table("Tsuruoka_ion.dat")
+	tmp.SurvTable$radiation = "ion"
+	SurvTable <- rbind( SurvTable , tmp.SurvTable )
+
+ return (SurvTable)
+}
+
+############ constants #########################
+
+SGP.survival.data.names <- function ( Z ){
+ if( Z == 1 ){
+ 	return ("proton")
+ } else if( Z == 6 ){
+ 	return ("carbon")
+ } else if( Z == 10 ){
+ 	return ("neon")
+ } else if( Z == 14 ){
+ 	return ("silicon")
+ } else if( Z == 26 ){
+ 	return ("iron")
+ } else {
+ 	return (Z)
+ }
+
+}
+
+# read data into data frames
+df.Survival = SGP.survival.data.read()
+
+cond  <- (df.Survival$radiation == "gamma")
+gamma.data <- data.frame( D.Gy = df.Survival$D.Gy[ cond ], Survival = df.Survival$Survival[ cond ], Sur.min = df.Survival$Sur.min[ cond ] , Sur.max = df.Survival$Sur.max[ cond ] , type = "gamma")   
+gamma.params <- SGP.fit.linquad( gamma.data$D.Gy, gamma.data$Survival, gamma.data$Sur.min, gamma.data$Sur.max, 0., 0. )
+
+cond <- paste( df.Survival$Z , df.Survival$Init.Energy, df.Survival$LET, sep="-")
+df.Survival$cond <- cond
+
+RBE.cond <- paste( df.Survival$Z[df.Survival$Z > 0] , df.Survival$Init.Energy[df.Survival$Z > 0], sep="-")
+df.Summary <- data.frame( Z = df.Survival$Z[df.Survival$Z > 0], LET = df.Survival$LET[df.Survival$Z > 0], Init.Energy = df.Survival$Init.Energy[df.Survival$Z > 0], RBE = 0) 
+df.Summary$cond <- RBE.cond
+
+df.Summary <- unique(df.Summary)
+
+RBE.vector <- numeric(0)
+
+# linear-quadratic fit, plots and calculations of RBE
+
+pdf("LQfit.pdf")
+
+# loop over all survival curves
+for( item in unique(df.Survival$cond) ){
+ if( item != "0-0-0" ){
+  cond <- ( df.Survival$cond == item )
+
+  # fitting of LQ equation to ion data
+  ion.params <- c(0.,0.)
+  try(ion.params <- SGP.fit.linquad( df.Survival$D.Gy[ cond ], df.Survival$Survival[ cond ], df.Survival$Sur.min[ cond ] , df.Survival$Sur.max[ cond ], 0., 0. ))
+	 
+	 # plot of gamma data
+	 df <- gamma.data
+	 plotCI( x = df$D.Gy , y = df$Survival/100., ui = df$Sur.max/100., li = df$Sur.min/100., gap = 0 , xlab = "Dose (Gy)" , ylab = "Surviving fraction" , log = "y" , axes = FALSE, pch = 22, xlim = c(0,6), ylim = c(0.001, 1.))
+	 
+	 # plot of LQ fit to gamma data
+  x <- seq( 0, 6, by=0.01)
+  y <- exp( -gamma.params[1]*x - gamma.params[2]*x*x)
+  lines( x = x , y = y )
+	 
+	 
+	 # plot of ion data 
+	 df <- data.frame( D.Gy = df.Survival$D.Gy[ cond ], Survival = df.Survival$Survival[ cond ], Sur.min = df.Survival$Sur.min[ cond ] , Sur.max = df.Survival$Sur.max[ cond ])
+	 par(new=TRUE)
+	 plotCI( x = df$D.Gy , y = df$Survival/100., ui = df$Sur.max/100., li = df$Sur.min/100., gap = 0 , xlab = "Dose (Gy)" , ylab = "Surviving fraction" , log = "y" , axes = FALSE, pch = 22, xlim = c(0,6), ylim = c(0.001, 1.))
+  legend( "topright", legend = c("200 kV X rays", paste( "LET = " , unique(df.Survival$LET[ cond ]) , "keV/um" ) ))
+  
+  # plot of LQ fit to ion data
+  x <- seq( 0, 6, by=0.01)
+  y <- exp( -ion.params[1]*x - ion.params[2]*x*x)
+  lines( x = x , y = y )
+  abline( h = c(0.1) )
+  axis( 1, at = c(0,1,2,3,4,5,6))
+  axis( 2, at = c(0.001,0.01,0.1,1))
+  
+  # calculation of RBE
+  RBE <- SGP.RBE( ion.params[1],  ion.params[2], gamma.params[1], gamma.params[2] )
+  RBE.vector <- c( RBE.vector , RBE)
+  title( main = substitute( paste( Zval, " ions, ", E[init], "=", Eval, "MeV/amu, ", alpha, "=", alphaval, " " ,beta, "=", betaval, " RBE = ", RBEval), list(Zval=SGP.survival.data.names(unique(df.Survival$Z[ cond ])) , Eval = unique(df.Survival$Init.Energy[ cond ]), alphaval = round(ion.params[1],3), betaval = round(ion.params[2],3), RBEval = round(RBE,2) ) ) )
+}	
+} 
+dev.off()
+
+df.Summary$RBE <- RBE.vector 
+
+pdf("RBE.pdf")
+
+p1 <- xyplot( RBE ~ LET | paste("Z =",Z,lapply( df.Summary$Z, SGP.survival.data.names)), data = df.Summary , groups = cond, auto.key = list(), type = "p" , scales = c( list( x = list(log = 10)) ))
+p1
+
+dev.off()
