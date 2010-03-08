0a77d1e35ceee308804fd49bcc58110d51a86b84
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-03-08 13:56:42 +0000

    Better implementation of Extended Target RDDs.

--- src/AT_RDD_ExtendedTarget.c
+++ src/AT_RDD_ExtendedTarget.c
@@ -54,22 +54,23 @@ void AT_RDD_Site_Gy( const long*  n,
 
 ///////////////////////////////////////////// EXTENDED TARGET /////////////////////////////////////////////
 
-double
-geometryFunctionPhi (const double r0_m,
+double          geometryFunctionPhi(         const double r_m,
     const double a0_m,
-    const double r_m)
+    const double t_m)
 {
+  // Phi(r,a0,t) = 2 arctan( sqrt( (a0^2 - (r-t)^2) / ((t + r)^2  - a0^2) ) )   for t > | r - a0 |
+  // Phi(r,a0,t) = pi                                                          for t <= | r - a0 |
   double res = 0.;
   double factor = 0.;
   gsl_complex carg, cres;
-  if (r_m <= fabs (r0_m - a0_m)){
-    if (r0_m >= a0_m)
+  if (t_m <= fabs (r_m - a0_m)){
+    if (r_m >= a0_m)
       res = 0.0;
     else
-      res = (double)M_PI;
+      res = M_PI;
   } else {
-    factor = gsl_pow_2 (a0_m) - gsl_pow_2 (r0_m - r_m);
-    factor /= gsl_pow_2 (r_m + r0_m) - gsl_pow_2 (a0_m);
+    factor = gsl_pow_2 (a0_m) - gsl_pow_2 (r_m - t_m);
+    factor /= gsl_pow_2 (t_m + r_m) - gsl_pow_2 (a0_m);
     GSL_SET_COMPLEX (&carg, sqrt (factor), 0.);
     cres = gsl_complex_arctan (carg);
     res = 2.0 * GSL_REAL (cres);
@@ -77,28 +78,28 @@ geometryFunctionPhi (const double r0_m,
   return res;
 }
 
-void AT_RDD_ExtendedTarget_Gy( const long*  n,
-    const float*  r_m,
-    const float*  a0_m,
+void AT_RDD_ExtendedTarget_Gy( const long  n,
+    const float* r_m,
+    const float  a0_m,
     /* radiation field parameters */
-    const float*  E_MeV_u,
-    const long*   particle_no,
+    const float  E_MeV_u,
+    const long   particle_no,
     /* detector parameters */
-    const long*   material_no,
+    const long   material_no,
     /* radial dose distribution model */
-    const long*   rdd_model,
-    const float*  rdd_parameter,
+    const long   rdd_model,
+    const float* rdd_parameter,
     /* electron range model */
-    const long*   er_model,
-    const float*  er_parameter,
-    float*        D_RDD_Gy){
+    const long   er_model,
+    const float* er_parameter,
+    float*       D_RDD_Gy){
 
   long  n_tmp    = 1;
 
   // Get material data
   float   density_g_cm3, density_kg_m3, electron_density_m3;
   AT_getMaterialData(  &n_tmp,
-              material_no,
+              &material_no,
               &density_g_cm3,
               &electron_density_m3,
               NULL, NULL, NULL, NULL, NULL, NULL);
@@ -109,11 +110,11 @@ void AT_RDD_ExtendedTarget_Gy( const long*  n,
   long  Z       =  0;
   float  Z_eff  =  0.0;
   AT_beta_from_E(  &n_tmp,
-                E_MeV_u,
+                &E_MeV_u,
                 &beta);
 
   AT_Z_from_particle_no(  &n_tmp,
-                particle_no,
+                &particle_no,
                 &Z);
   AT_effective_charge_from_beta(  &n_tmp,
                   &beta,
@@ -123,9 +124,9 @@ void AT_RDD_ExtendedTarget_Gy( const long*  n,
   // Get the maximum electron range
   float  max_electron_range_m = 0.0f;
   AT_max_electron_range_m(  n_tmp,
-                E_MeV_u,
-                *material_no,
-                *er_model,
+                &E_MeV_u,
+                material_no,
+                er_model,
                 &max_electron_range_m);
 
   const float C_J_m               =  AT_RDD_Katz_C_J_m(electron_density_m3);
@@ -133,9 +134,9 @@ void AT_RDD_ExtendedTarget_Gy( const long*  n,
   // Get LET
   float  LET_MeV_cm2_g  =  0.0f;
   AT_LET_MeV_cm2_g(  &n_tmp,
-            E_MeV_u,
-            particle_no,
-            material_no,
+            &E_MeV_u,
+            &particle_no,
+            &material_no,
             &LET_MeV_cm2_g);
 
   float  LET_J_m        =  LET_MeV_cm2_g * density_g_cm3; // [MeV / cm]
@@ -143,8 +144,17 @@ void AT_RDD_ExtendedTarget_Gy( const long*  n,
   LET_J_m              *=  MeV_to_J;     // [J/m]
 
   float Katz_point_coeff_Gy =  AT_RDD_Katz_coeff_Gy(C_J_m,Z_eff,beta,density_kg_m3,max_electron_range_m);
-  float r_max_m = GSL_MIN((double)(*a0_m), max_electron_range_m);
+  float r_max_m = GSL_MIN((double)a0_m, max_electron_range_m);
 
+  float alpha           =  0.0f;
+  if( (er_model == ER_Waligorski) || (er_model == ER_Edmund) ){ // "new" Katz RDD
+    alpha               =  1.667f;
+    float wmax_MeV      =  0.0f;
+    AT_max_E_transfer_MeV(&n_tmp,&E_MeV_u,&wmax_MeV);
+    if(wmax_MeV <= 1e-3){
+      alpha             =  1.079f;
+    }
+  }
 
   /********************************************************
    ****************** LOOP OVER DISTANCES *****************
@@ -153,118 +163,134 @@ void AT_RDD_ExtendedTarget_Gy( const long*  n,
 
   float r_min_m = rdd_parameter[0];
 
-  for( i = 0 ; i < *n ; i++){
+  for( i = 0 ; i < n ; i++){
 
-    if( *rdd_model == RDD_KatzPoint){
+    D_RDD_Gy[i] = 0.0f;
 
-      if( (*er_model == ER_Waligorski) || (*er_model == ER_Edmund) ){ // "new" Katz RDD
-        float alpha         =  1.667f;
-        float wmax_MeV      =  0.0f;
-        AT_max_E_transfer_MeV(&n_tmp,E_MeV_u,&wmax_MeV);
-        if(wmax_MeV <= 1e-3){
-          alpha             =  1.079f;
-        }
+    if( rdd_model == RDD_KatzPoint){ // TODO write comments
 
-        D_RDD_Gy[i] = 0.0f;
+      if( (er_model == ER_Waligorski) || (er_model == ER_Edmund) ){ // "new" Katz RDD
 
-        if( (r_m[i] <=  0.01 * (*a0_m)) && (r_m[i] >=  r_min_m)){
+        if( (r_m[i] <=  0.01 * a0_m) && (r_m[i] >=  r_min_m)){
           D_RDD_Gy[i] = AT_RDD_Katz_PowerLawER_Daverage_Gy( r_min_m, r_max_m, max_electron_range_m, alpha, Katz_point_coeff_Gy );
+          if( max_electron_range_m < a0_m ){
+            D_RDD_Gy[i] *= (gsl_pow_2(r_max_m) - gsl_pow_2(r_min_m));
+            D_RDD_Gy[i] /= (gsl_pow_2(a0_m) - gsl_pow_2(r_min_m));
+          }
         }
 
-        if( (r_m[i] >=  100.0 * (*a0_m)) && (r_m[i] <= max_electron_range_m) ){
+        if( (r_m[i] >=  100.0 * a0_m) && (r_m[i] <= max_electron_range_m) ){
           D_RDD_Gy[i] = AT_RDD_Katz_PowerLawER_Dpoint_Gy( r_m[i], alpha, max_electron_range_m, Katz_point_coeff_Gy );
         }
 
-        if( (r_m[i] <  100.0 * (*a0_m)) && (r_m[i] >  0.01 * (*a0_m)) ){
-          D_RDD_Gy[i] = 0.0; // TODO integration
+        if( (r_m[i] <  100.0 * a0_m) && (r_m[i] >  0.01 * a0_m) ){
+          D_RDD_Gy[i] = AT_RDD_ExtendedTarget_integrate_Gy(r_m[i],a0_m,r_min_m,r_max_m,E_MeV_u,particle_no,material_no,rdd_model,rdd_parameter,er_model,er_parameter);
         }
 
+        // end if ER_Waligorski, ER_Edmund
+      } else if (er_model == ER_ButtsKatz){ // "old" Katz RDD
 
-      } else if (*er_model == ER_ButtsKatz){ // "old" Katz RDD
-
-        if( (r_m[i] <=  0.01 * (*a0_m)) && (r_m[i] >=  r_min_m)){
+        if( (r_m[i] <=  0.01 * a0_m) && (r_m[i] >=  r_min_m)){
           D_RDD_Gy[i] = AT_RDD_Katz_LinearER_Daverage_Gy( r_min_m, r_max_m, max_electron_range_m, Katz_point_coeff_Gy );
+          if( max_electron_range_m < a0_m ){
+            D_RDD_Gy[i] *= (gsl_pow_2(r_max_m) - gsl_pow_2(r_min_m));
+            D_RDD_Gy[i] /= (gsl_pow_2(a0_m) - gsl_pow_2(r_min_m));
+          }
         }
-        if( (r_m[i] >=  100.0 * (*a0_m)) && (r_m[i] <= max_electron_range_m) ){
+
+        if( (r_m[i] >=  100.0 * a0_m) && (r_m[i] <= max_electron_range_m) ){
           D_RDD_Gy[i] = AT_RDD_Katz_LinearER_Dpoint_Gy( r_m[i], max_electron_range_m, Katz_point_coeff_Gy );
         }
-        if( (r_m[i] <  100.0 * (*a0_m)) && (r_m[i] >  0.01 * (*a0_m)) ){
-          D_RDD_Gy[i] = 0.0f;  // TODO integration
+
+        if( (r_m[i] <  100.0 * a0_m) && (r_m[i] >  0.01 * a0_m) ){
+          D_RDD_Gy[i] = AT_RDD_ExtendedTarget_integrate_Gy(r_m[i],a0_m,r_min_m,r_max_m,E_MeV_u,particle_no,material_no,rdd_model,rdd_parameter,er_model,er_parameter);
         }
 
-
-
+        // end if ER_ButtsKatz
       } else {
         D_RDD_Gy[i] = 0.0f;
       }
 
-      if( max_electron_range_m < (*a0_m) ){
-        D_RDD_Gy[i] *= (gsl_pow_2(r_max_m) - gsl_pow_2(r_min_m));
-        D_RDD_Gy[i] /= (gsl_pow_2(*a0_m) - gsl_pow_2(r_min_m));
-      }
-
+    } else { // RDD other that RDD_KatzPoint
+      D_RDD_Gy[i] = AT_RDD_ExtendedTarget_integrate_Gy(r_m[i],a0_m,r_min_m,r_max_m,E_MeV_u,particle_no,material_no,rdd_model,rdd_parameter,er_model,er_parameter);
     }
 
-  }
+  }// end loop over r_m
+
 }
 
-inline double AT_RDD_Katz_ext_kernel_Gy(const double t_m,
-    const double r_m,
-    const double a0_m,
-    const double alpha,
-    const double r_min_m,
-    const double r_max_m,
-    const double Katz_point_coeff_Gy){
-
-  if( t_m < r_min_m )
-    return 0.0;
-  if( t_m >= a0_m + r_m )
-    return 0.0;
-  if( (r_m >= a0_m) && (t_m <= r_m - a0_m))
-    return 0.0;
-  else
-    return  (M_1_PI/ (gsl_pow_2(a0_m))) * AT_RDD_Katz_PowerLawER_Dpoint_Gy(t_m,alpha,r_max_m,Katz_point_coeff_Gy) *  geometryFunctionPhi(r_m,a0_m,t_m) * t_m;
-}
 
 double AT_RDD_Katz_ext_integrand_Gy(  double t_m,
     void * params){
 
-  double r_m           = ((double*)params)[0];
-  double a0_m           = ((double*)params)[1];
-  double alpha         = ((double*)params)[2];
-  double r_min_m         = ((double*)params)[3];
-  double r_max_m         = ((double*)params)[4];
-  double Katz_point_coeff_Gy   = ((double*)params)[5];
-  double f_t_m         = (double)(t_m);
-  return (double)AT_RDD_Katz_ext_kernel_Gy(  f_t_m, r_m, a0_m, alpha,
-      r_min_m, r_max_m, Katz_point_coeff_Gy);
+  AT_RDD_ExtendedTarget_parameters* RDD_parameters = (AT_RDD_ExtendedTarget_parameters*)params;
+
+  const long   n              =  1;
+  const float  r_m            =  RDD_parameters->r_m;
+  const float  a0_m           =  RDD_parameters->a0_m;
+  const float  E_MeV_u        =  RDD_parameters->E_MeV_u;
+  const long   particle_no    =  RDD_parameters->particle_no;
+  const long   material_no    =  RDD_parameters->material_no;
+  const long   rdd_model      =  RDD_parameters->rdd_model;
+  const float* rdd_parameter  =  RDD_parameters->rdd_parameter;
+  const long   er_model       =  RDD_parameters->er_model;
+  const float* er_parameter   =  RDD_parameters->er_parameter;
+
+  float D_Gy;
+  float t_m_float = (float)t_m;
+  AT_D_RDD_Gy( &n, &t_m_float, &E_MeV_u,  &particle_no, &material_no,  &rdd_model,  rdd_parameter,  &er_model,  er_parameter,  &D_Gy);
+
+  return 2.0 * t_m * (double)D_Gy * geometryFunctionPhi(r_m,a0_m,t_m);
 }
 
-inline double AT_RDD_Katz_ext_Gy(  const double r_m,
+double AT_RDD_ExtendedTarget_integrate_Gy(  const double r_m,
     const double a0_m,
-    const double alpha,
     const double r_min_m,
     const double r_max_m,
-    const double Katz_point_coeff_Gy){
+    const float  E_MeV_u,
+    const long   particle_no,
+    /* detector parameters */
+    const long   material_no,
+    /* radial dose distribution model */
+    const long   rdd_model,
+    const float* rdd_parameter,
+    /* electron range model */
+    const long   er_model,
+    const float* er_parameter){
 
-  double int_lim_m = 0.0f;
+  double low_lim_m = 0.0;
   if( r_m > a0_m ){
-    int_lim_m = GSL_MAX(r_m - a0_m,r_min_m);
+    low_lim_m = GSL_MAX(r_m - a0_m,r_min_m);
   }
   gsl_set_error_handler_off();
 
   double ext_integral_Gy;
   double error;
-  gsl_integration_workspace *w1 = gsl_integration_workspace_alloc (10000);
+  gsl_integration_workspace *w1 = gsl_integration_workspace_alloc (1000);
   gsl_function F;
   F.function = &AT_RDD_Katz_ext_integrand_Gy;
-  double params[] = {r_m,a0_m,alpha,r_min_m,r_max_m,Katz_point_coeff_Gy};
-  F.params = params;
-  int status = gsl_integration_qags (&F, int_lim_m, r_m+a0_m, 1e-9, 1e-4, 10000, w1, &ext_integral_Gy, &error);
+
+  AT_RDD_ExtendedTarget_parameters RDD_parameters;
+
+  RDD_parameters.r_m           =  r_m;
+  RDD_parameters.a0_m          =  a0_m;
+  RDD_parameters.E_MeV_u       =  E_MeV_u;
+  RDD_parameters.particle_no   =  particle_no;
+  RDD_parameters.material_no   =  material_no;
+  RDD_parameters.rdd_model     =  rdd_model;
+  RDD_parameters.rdd_parameter =  rdd_parameter;
+  RDD_parameters.er_model      =  er_model;
+  RDD_parameters.er_parameter  =  er_parameter;
+
+  F.params = (void*)(&RDD_parameters);
+  int status = gsl_integration_qags (&F, low_lim_m, r_m+a0_m, 0, 1e-5, 1000, w1, &ext_integral_Gy, &error);
   if (status == GSL_EROUND || status == GSL_ESING){
+    printf("Error\n");
     ext_integral_Gy = -1.0;
   }
   gsl_integration_workspace_free (w1);
+
+  ext_integral_Gy *= M_1_PI / gsl_pow_2(a0_m);
 
   return ext_integral_Gy;
 }
