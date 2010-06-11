ba6796ad29baf528f077e4723cc4e2043ff75c8f
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-06-11 15:50:41 +0000

    Testing unit for GSM algorithm.

---
+++ test/C/AT_test_GSM.c
@@ -0,0 +1,46 @@
+/*
+ * @file
+ * @brief Dummy file to enable debugging, to be changed by the user anyway they like.
+ */
+
+/*
+ *    AT_test_GSM.c
+ *    ===================
+ *
+ *    Created on: 2010-06-11
+ *    Author: grzanka
+ *
+ *    Copyright 2006, 2009 Steffen Greilich / the libamtrack team
+ *
+ *    This file is part of the AmTrack program (libamtrack.sourceforge.net).
+ *
+ *    AmTrack is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    AmTrack is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with AmTrack (file: copying.txt).
+ *    If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "AmTrack.h"
+
+int main( int argc, char * argv[] ){
+
+	printf("Test of GSM algorithm\n");
+
+	return 0;
+};
