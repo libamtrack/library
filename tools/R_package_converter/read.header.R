99d0fd33dfb127281ef8fc0357f70174a360536b
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-09-29 12:24:54 +0000

    Code cleaned, license comment added.

--- tools/R_package_converter/read.header.R
+++ tools/R_package_converter/read.header.R
@@ -1,4 +1,25 @@
-try(setwd("C:/Data/__Studium/01-R_test/wrapper"), silent = T)
+################################################################################################
+# C wrapper generator
+################################################################################################
+# Copyright 2006, 2009 Steffen Greilich / the libamtrack team
+# 
+# This file is part of the AmTrack program (libamtrack.sourceforge.net).
+#
+# AmTrack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# AmTrack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# long with AmTrack (file: copying.txt).
+# If not, see <http://www.gnu.org/licenses/>
+################################################################################################
+
 
 # save current working directory
 cur.dir <- getwd()
@@ -70,7 +91,7 @@ for(file in record){
 	include <- include[keep]
 
 	#############################################
-	# separate the doxgen comment and the code
+	# separate the doxygen comment and the code
 	#############################################
 
 	# all vectors should be of same length
--- tools/R_package_converter/type.conversion.R
+++ tools/R_package_converter/type.conversion.R
@@ -1,13 +1,37 @@
+################################################################################################
+# C wrapper generator
+################################################################################################
+# Copyright 2006, 2009 Steffen Greilich / the libamtrack team
+# 
+# This file is part of the AmTrack program (libamtrack.sourceforge.net).
+#
+# AmTrack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# AmTrack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# long with AmTrack (file: copying.txt).
+# If not, see <http://www.gnu.org/licenses/>
+################################################################################################
+
+
 types <- c("bool", "long", "int", "long", "double")
 conversion.c <- c("int*\t", "int*\t", "int*\t", "long*\t", "float*")
 conversion.R <- c("as.integer", "as.integer", "as.integer", "as.integer", "as.single")
 extension <- c("_long", "_long", "_long", "_long", "_double")
 
-conversion.table <- data.frame(	type = c(types, paste("const", types)),
+conversion.table <- data.frame(	type = c(types, paste("const", types), paste(types,"*",sep = "")),
 						conversion.c = c(paste(conversion.c, "\t", sep = ""),
-									 paste("const", conversion.c)),
-						conversion.R = c(conversion.R, conversion.R),
-						extension = c(extension, extension),
+									 paste("const", conversion.c),
+									 paste(conversion.c)),
+						conversion.R = c(conversion.R, conversion.R, conversion.R),
+						extension = c(extension, extension, extension),
 						stringsAsFactors = F
 					)
 
