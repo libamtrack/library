057e7534245cc71a8c9257c8ece71b88d33704b4
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-10-04 14:31:53 +0000

    Deprecated function removed.

--- trunk/wrapper/Java/src/runme.java
+++ trunk/wrapper/Java/src/runme.java
@@ -7,8 +7,6 @@ public class runme {
 
   public static void main(String argv[]) {
     System.out.println(example.fact(4));
-    example.AT_PrintName();
-    System.out.println(example.AT_GetNumber());
   }
 }
 
