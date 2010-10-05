7c00ce3921f7a1dc743f58b49fa9a1e772ed23e2
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-10-05 09:04:43 +0000

    Doxygen documentation improved.

--- trunk/include/AT_Histograms.h
+++ trunk/include/AT_Histograms.h
@@ -33,64 +33,160 @@
 #include <math.h>
 
 /**
- * @enum Histogram type enumerator
+ * @enum AT_histo_type
+ * Histogram type enumerator
  */
 enum AT_histo_type{
   AT_histo_linear       = 0, /** Histogram with linear bins and arithmetic mid-points **/
   AT_hiso_log           = 1  /** Histogram with logarithmic bins and geometrical mid-points **/
 };
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param left_limits
+ * @param bin_widths
+ */
 void AT_histo_bin_width(        const long number_of_bins,
                                 const double left_limits[],
                                 double bin_widths[]);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param left_limits
+ * @param histo_type
+ * @param midpoints
+ */
 void AT_histo_midpoints(        const long number_of_bins,
                                 const double left_limits[],
                                 const long histo_type,
                                 double midpoints[]);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param left_limits
+ * @param midpoints
+ */
 void AT_histo_arithmetic_midpoints( const long number_of_bins,
     const double left_limits[],
     double midpoints[]);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param left_limits
+ * @param midpoints
+ */
 void AT_histo_geometric_midpoints( const long number_of_bins,
     const double left_limits[],
     double midpoints[]);
 
 
+/**
+ * TODO
+ * @param number_of_bins
+ * @param midpoints
+ * @param histo_type
+ * @param left_limits
+ */
 void AT_histo_left_limits( const long number_of_bins,
     const double midpoints[],
     const long histo_type,
     double left_limits[]);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param midpoints
+ * @param left_limits
+ */
 void AT_histo_arithmetic_left_limits( const long number_of_bins,
     const double midpoints[],
     double left_limits[]);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param midpoints
+ * @param left_limits
+ */
 void AT_histo_geometric_left_limits( const long number_of_bins,
     const double midpoints[],
     double left_limits[]);
 
 /* OLD ROUTINES, KEPT FOR COMPATIBILITY */
+/**
+ * TODO
+ * @param number_of_bins
+ * @param bin_centers
+ * @return
+ */
 double AT_histo_log_bin_width(	const long number_of_bins,
 								const double bin_centers[]);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param bin_centers
+ * @param bin_no
+ * @return
+ */
 double AT_histo_lower_bin_limit(const long number_of_bins,
 								const double bin_centers[],
 								const long bin_no);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param bin_centers
+ * @param bin_no
+ * @return
+ */
 double AT_histo_upper_bin_limit(const long number_of_bins,
 								const double bin_centers[],
 								const long bin_no);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param bin_centers
+ * @param bin_no
+ * @return
+ */
 double AT_histo_get_bin_width(	const long number_of_bins,
 								const double bin_centers[],
 								const long bin_no);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param bin_centers
+ * @param bin_widths
+ */
 void AT_histo_get_bin_widths(	const long number_of_bins,
 								const double bin_centers[],
 								double bin_widths[]);
 
+
+/**
+ * TODO
+ * @param number_of_bins
+ * @param bin_centers
+ * @param value
+ * @return
+ */
 long AT_histo_bin_no(	const long number_of_bins,
 						const double bin_centers[],
 						const double value);
