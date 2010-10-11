3efbddd178254c3e498c8e5c54037a6d6eb21418
kleinf <kleinf@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-10-11 07:07:16 +0000

    Changed to work with new "read.header.R" output format

--- trunk/tools/R_package_converter/convert.doxygen.to.Rd.R
+++ trunk/tools/R_package_converter/convert.doxygen.to.Rd.R
@@ -31,27 +31,27 @@ source("type.conversion.R")
 load("functions.ssd")
 
 for(i in 1:length(functions)){
-	tmp <- functions[[i]]$comment
+	tmp <- functions[[i]]
 
 	# replace "_" by "."
 	tmp$name <- gsub("_", ".", tmp$name)
-	tmp$parameter$name <- gsub("_", ".", tmp$parameter$name)
+	tmp$parameter.comment$name <- gsub("_", ".", tmp$parameter.comment$name)
 
 	# remove "[]"
-	tmp$parameter$name <- gsub("[]", "", tmp$parameter$name, fixed = T)
+	tmp$parameter.comment$name <- gsub("[]", "", tmp$parameter.comment$name, fixed = T)
 
 	# paste all input parameters together
-	ii <- which(grepl("in", tmp$parameter$type) & tmp$parameter$name != "n")
+	ii <- which(grepl("in", tmp$parameter.comment$type) & tmp$parameter.comment$name != "n")
 	parameter.list <- character(0)
 	if(length(ii) > 0){ 
 		if(length(ii) > 1){ 
 			for(i in ii[1:(length(ii) - 1)]){
-				parameter.list <- paste(parameter.list, tmp$parameter$name[i], ", ", sep = "") 
+				parameter.list <- paste(parameter.list, tmp$parameter.comment$name[i], ", ", sep = "") 
 				if(which(i == ii)%%5 == 0) parameter.list <- paste(parameter.list, "\n\t", sep = "") 
 
 			}
 		}
-		parameter.list <- paste(parameter.list, tmp$parameter$name[ii[length(ii)]], sep = "") 
+		parameter.list <- paste(parameter.list, tmp$parameter.comment$name[ii[length(ii)]], sep = "") 
 	}
 
 	write("// Automatically created Rd file\n", file = paste(tmp$name, ".Rd", sep = ""))
@@ -76,7 +76,7 @@ for(i in 1:length(functions)){
 	# start arguments
 	header <- c(header, "\\arguments{")
 
-	para <- tmp$parameter
+	para <- tmp$parameter.comment
 	para.in <- which(grepl("in", para$type) & para$name != "n")
 	if(length(para.in) > 0){
 		for(i in para.in){
@@ -101,8 +101,9 @@ for(i in 1:length(functions)){
 
 	write(c(header, "\n"), file = paste(tmp$name, ".Rd", sep = ""), append = T)
 }
-tmp
-header
+
+#tmp
+#header
 
 
 
