247441f1c82a416fe4faa4c73fceb0ea72ebe11d
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-10-13 09:48:15 +0000

    Updated initial.R to enable automatic loading of share library

--- trunk/tools/R_package_converter/package/R/initial.R
+++ trunk/tools/R_package_converter/package/R/initial.R
@@ -1,1 +1,14 @@
-print("This is libamtrack")
+.First.lib <- function(lib, pkg){
+ library.dynam("libamtrack", pkg, lib)
+ print("This is libamtrack (2010-10-13).\nThe lattice package will be loaded automatically.")
+ require(lattice)
+}
+
+.Last.lib <- function(lib, pkg){
+ library.dynam.unload("libamtrack", pkg, lib)
+ pos <- match("package:lattice", search())
+ if(! is.na(pos)){
+  detach(pos = pos)
+ }
+}
+
