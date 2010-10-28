8e3e33651b11004a849edc02a64086cd09665860
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-10-28 16:45:31 +0000

    Added more functions to the R package, incl. examples

--- trunk/tools/R_package_converter/package/R/initial.R
+++ trunk/tools/R_package_converter/package/R/initial.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg){
  library.dynam("libamtrack", pkg, lib)
- cat("This is libamtrack 0.3 (2010-10-22). Type '?libamtrack' for help.\n")
+ cat("This is libamtrack 0.4.0 (2010-10-29). Type '?libamtrack' for help.\n")
 }
 
 .Last.lib <- function(libpath){
