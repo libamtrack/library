b41680dd0aaa1483daa7c5727e1141560141aa77
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-10-30 15:33:35 +0000

    R Package version incremented.

--- trunk/tools/R_package_converter/create.package.sh
+++ trunk/tools/R_package_converter/create.package.sh
@@ -9,12 +9,12 @@ R --no-save < convert.header.to.cwrapper.R
 R --no-save < convert.header.to.Rwrapper.R
 R --no-save < convert.doxygen.to.Rd.R
 R CMD build package # this command produce .tar.gz file which is not a R package
-R CMD check libamtrack_0.3.tar.gz --no-latex # this command produce directory with R package
-rm libamtrack_0.3.tar.gz # let us remove this file as this is not really a R package
+R CMD check libamtrack_0.4.1.tar.gz --no-latex # this command produce directory with R package
+rm libamtrack_0.4.1.tar.gz # let us remove this file as this is not really a R package
 rm package/src/*.h
 rm package/src/*.c
 cd libamtrack.Rcheck
-tar -zcf libamtrack_0.3.tar.gz libamtrack/ # packaging of real package
-mv libamtrack_0.3.tar.gz ../
+tar -zcf libamtrack_0.4.1.tar.gz libamtrack/ # packaging of real package
+mv libamtrack_0.4.1.tar.gz ../
 rm -rf libamtrack.Rcheck
 cd ../..
