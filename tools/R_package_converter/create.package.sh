0fd774727c068c9533b28624ec90cf7e7cb83b59
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-11-01 19:45:26 +0000

    New linux R package generation script

--- tools/R_package_converter/create.package.sh
+++ tools/R_package_converter/create.package.sh
@@ -4,17 +4,19 @@ cp ../../include/*.h package/src/
 cp ../../src/*.c package/src/
 rm package/src/AT_Wrapper_R.c
 rm package/src/AT_Wrapper_R.h
+cp hardcoded_documentation/* package/man/
+cp hardcoded_wrappers/hardcoded_wrapper.R package/R/
+cp hardcoded_wrappers/hardcoded_wrapper.c package/src/
+cp hardcoded_wrappers/hardcoded_wrapper.h package/src/
 R --no-save < read.header.R
 R --no-save < convert.header.to.cwrapper.R
 R --no-save < convert.header.to.Rwrapper.R
 R --no-save < convert.doxygen.to.Rd.R
-R CMD build package # this command produce .tar.gz file which is not a R package
-R CMD check libamtrack_0.4.1.tar.gz --no-latex # this command produce directory with R package
-rm libamtrack_0.4.1.tar.gz # let us remove this file as this is not really a R package
+R CMD check ./package --no-latex # this command produce directory with R package
 rm package/src/*.h
 rm package/src/*.c
-cd libamtrack.Rcheck
+cd package.Rcheck
 tar -zcf libamtrack_0.4.1.tar.gz libamtrack/ # packaging of real package
 mv libamtrack_0.4.1.tar.gz ../
-rm -rf libamtrack.Rcheck
+rm -rf package.Rcheck
 cd ../..
