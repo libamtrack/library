843c9bdbbc3330504f2dd42da93437463b5e8e5d
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-11-02 08:12:41 +0000

    Renumbering of tags

---
+++ src/SGParticle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _LINUX // [_LINUX or _WINDOWS] : in Linux we have isnan function while in Windows we have _isnan
+#define _R // [_S or _R] in S we can pass long type to the function via as.single, but in R we pass int type
+
+#include "SGP_Constants.h"
+#include "SGP_Data.h"
+#include "SGP_Functions.h"
+#include "SGP_RDD.h"
+#include "SGP_SuccessiveConvolutions.h"
+#include "SGP_GammaResponse.h"
+#include "SGP_FileOperations.h"
+#include "SGP_Transport.h"
+
+/*
+BOOL APIENTRY DllMain( HANDLE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+    return TRUE;
+}
+*/
+
