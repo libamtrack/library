054c4e561d1003bad6d0ec6451635b066f60fd8f
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-11-06 22:20:13 +0000

    More particles added, libamtrack logo added, close current calculation button added.

--- tools/webgui/AmtrackService/src/amtrackservice/client/gui/MainView.java
+++ tools/webgui/AmtrackService/src/amtrackservice/client/gui/MainView.java
@@ -1,67 +1,115 @@
 package amtrackservice.client.gui;
 
 import amtrackservice.client.AmtrackService;
+import amtrackservice.client.AmtrackService.AmtrackServiceResources;
 import amtrackservice.client.Logger;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.client.Command;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.DecoratedStackPanel;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
-import com.google.gwt.user.client.ui.MenuBar;
-import com.google.gwt.user.client.ui.MenuItem;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.user.client.ui.SplitLayoutPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
-import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
- * 
  * This Represents the GUI 
  * @author Christoph Kolb, 2010 Hochschule Heilbronn
  */
 public class MainView {
 
-	private AmtrackService service;
 	private TabLayoutPanel tabPanel;
-	private TextArea console;
-	private MenuBar menubar;
-	private MenuBar menuNew;
 	private DecoratedStackPanel leftPanel;
 
 	/**
 	 * Constructor of MainView
-	 * @param service the corresponding LibService
+	 * @param service 
 	 */
 	public MainView(AmtrackService service) {
-		this.service = service;
 		Logger.init(this);
-		console = new TextArea();
 		
-		menubar = new MenuBar();
-		menuNew = new MenuBar(true);
-		//menubar.addItem("New calculation", menuNew);
-		//menubar.addSeparator();
-		menubar.addItem("Close current calculation", new CalculationClose());
+		AmtrackServiceResources resources = GWT.create(AmtrackServiceResources.class);
+	    ClickHandler closeHandler = new ClickHandler() {					
+			@Override
+			public void onClick(ClickEvent event) {
+				int index = tabPanel.getSelectedIndex();
+				tabPanel.remove(index);
+			}
+		};
+		
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+		horizontalPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
 
-		tabPanel = new TabLayoutPanel(10, Unit.MM);
-		
-		SplitLayoutPanel rootPanel = new SplitLayoutPanel();
+		VerticalPanel westVerticalPanel = new VerticalPanel();
+		westVerticalPanel.setWidth("300px");
+		westVerticalPanel.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+
+		Image logoImage = new Image(resources.logo());
+		logoImage.setSize("300px", "200px");
 		
 	    leftPanel = new DecoratedStackPanel();
 	    leftPanel.setWidth("300px");
 
+	    VerticalPanel verticalPanel_1 = new VerticalPanel();
+	    verticalPanel_1.setSize("1000px", "");
+
+	    HorizontalPanel horizontalPanel_1 = new HorizontalPanel();
+	    horizontalPanel_1.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+	    horizontalPanel_1.setSize("1000px", "32px");
 	    
-	    rootPanel.addWest(leftPanel,300);
-		DockLayoutPanel main = new DockLayoutPanel(Unit.MM);
-		main.addNorth(menubar, 5);
-		main.add(tabPanel);
-		
-		rootPanel.add(main);
-		
+	    Image closeImage = new Image(resources.close());
+	    closeImage.setSize("32px", "32px");
+
+	    HTML htmlCloseCurrentCalculation = new HTML("Close current calculation: ", true);
+	    htmlCloseCurrentCalculation.setStyleName("h1");
+	    htmlCloseCurrentCalculation.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+	    htmlCloseCurrentCalculation.setSize("200px", "32px");
+	    
+	    PushButton closeButton = new PushButton(closeImage,closeHandler);
+	    closeButton.setSize("32px", "32px");
+
+	    tabPanel = new TabLayoutPanel(10, Unit.MM);
+	    tabPanel.setSize("1000px", "800px");
+	    
 		RootLayoutPanel rp  = RootLayoutPanel.get();
-		rp.add(rootPanel);
+		rp.setSize("1300px", "1300px");
 		
+		rp.add(horizontalPanel);
+		rp.setWidgetLeftWidth(horizontalPanel, 0.0, Unit.PX, 1800.0, Unit.PX);
+		rp.setWidgetTopHeight(horizontalPanel, 0.0, Unit.PX, 1800.0, Unit.PX);
+		
+		horizontalPanel.add(westVerticalPanel);		
+    
+	    westVerticalPanel.add(logoImage);
+	    westVerticalPanel.add(leftPanel);
+	    westVerticalPanel.setCellHeight(logoImage, "200px");
+	    westVerticalPanel.setCellVerticalAlignment(logoImage, HasVerticalAlignment.ALIGN_BOTTOM);
+	    westVerticalPanel.setCellHorizontalAlignment(logoImage, HasHorizontalAlignment.ALIGN_CENTER);
+	    westVerticalPanel.setCellWidth(leftPanel, "300px");
+	    westVerticalPanel.setCellHorizontalAlignment(leftPanel, HasHorizontalAlignment.ALIGN_CENTER);
+
+	    horizontalPanel.add(verticalPanel_1);	    
+	    verticalPanel_1.add(horizontalPanel_1);	    
+	    verticalPanel_1.setCellHorizontalAlignment(horizontalPanel_1, HasHorizontalAlignment.ALIGN_RIGHT);
+	    	    
+	    horizontalPanel_1.add(htmlCloseCurrentCalculation);
+	    horizontalPanel_1.setCellHorizontalAlignment(htmlCloseCurrentCalculation, HasHorizontalAlignment.ALIGN_RIGHT);
+	    horizontalPanel_1.setCellHeight(htmlCloseCurrentCalculation, "32px");
+	    horizontalPanel_1.setCellVerticalAlignment(htmlCloseCurrentCalculation, HasVerticalAlignment.ALIGN_MIDDLE);
+	    horizontalPanel_1.add(closeButton);
+	    horizontalPanel_1.setCellHeight(closeButton, "32px");
+	    horizontalPanel_1.setCellWidth(closeButton, "32px");
+	    horizontalPanel_1.setCellHorizontalAlignment(closeButton, HasHorizontalAlignment.ALIGN_RIGHT);
+
+	    verticalPanel_1.add(tabPanel);
 	}
 	
 	/**
@@ -76,45 +124,5 @@ public class MainView {
 	
 	public void addWidgetToLeftPanel(Widget w, String description){
 		leftPanel.add(w,description,true);
-	}
-	
-	/**
-	 * 
-	 * @param line
-	 */
-	public void addConsoleLine(String line){
-		console.setText(console.getText()+"\n"+line);
-		console.getElement().setScrollTop(console.getElement().getScrollHeight());
-	}
-	
-	private class CalculationStart implements Command{
-
-		private String method;
-		
-		public CalculationStart(String method) {
-			this.method = method;
-		}
-		@Override
-		public void execute() {
-			service.openCalculation(method);
-			
-		}
-		
-	}
-	
-	private class CalculationClose implements Command{		
-		@Override
-		public void execute() {
-			int index = tabPanel.getSelectedIndex();
-			tabPanel.remove(index);
-		}
-		
-	}
-
-	public void initMenu(String[] array) {
-		for(String m : array){
-			MenuItem mi = new MenuItem(m, new CalculationStart(m));
-			menuNew.addItem(mi);
-		}			
 	}
 }
