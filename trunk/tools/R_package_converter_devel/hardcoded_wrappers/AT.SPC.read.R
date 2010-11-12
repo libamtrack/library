a289b370bd74d201f85e1ceb54a2dd1abff54c6c
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-11-12 14:00:58 +0000

    Optimized AT.SPC.read routine for speed

--- trunk/tools/R_package_converter_devel/hardcoded_wrappers/AT.SPC.read.R
+++ trunk/tools/R_package_converter_devel/hardcoded_wrappers/AT.SPC.read.R
@@ -15,212 +15,134 @@
 #
 # revised 2010/11/02, sgre
 # added to R package
-
-
+#
+# revised 2010/11/10, sgre
+# switched to matrix ops instead of dataframes, optimized for libamtrack, removed raw option
+#
+# revised 2010/11/11, sgre
+# speed-up due to working on index tables, prototype for C version
 AT.SPC.read <- function( file.name, 
                          endian         = c("big", "little")[1], 
-                         mean           = c(geometric = 0, arithmetic = 1)[2],
-                         raw            = FALSE,
+                         mean           = c("geometric", "arithmetic")[2],
                          compress       = TRUE)
 {
-    ###########################
-    # DEFINE INTERNAL FUNCTIONS
-    ###########################
-
-    # 1. function for reading tags
-    read.tag    <-    function(    file.handle, endian){
-        tag         <-    readBin(file.handle, integer(), endian = endian)
-        length      <-    readBin(file.handle, integer(), endian = endian)
-        return(list(    tag       =    tag,
-                        length    =    length))
-    }
-
-    # 2. tag codes
-    SPC.tags    <-    data.frame(    tag        =    1:20,    
-                        TRiP.name    =    c(    "FILETYPE",            "FILEVERSION",            "FILEDATE",            "TARGNAME",                    "PROJNAME",
-                                        "B",                "P",                    "N",                "NZ",                        "Z",
-                                        "N",                "NS",                    "S",                "CUM",                    "NC",
-                                        "NE",                "E",                    "EREF",            "HISTO",                    "RUNNINGSUM"),
-                        real.name    =    c(    "file.type",        "file.version",            "file.date",        "target.name",                "projectile.name",
-                                        "beam.energy.MeV.u",    "peak.position.g.cm2",        "normalization",        "number.of.depth.steps",        "depth.g.cm2",
-                                        "normalization",        "number.of.particle.species",    "Z.and.A",            "cumulated.number.of.fragments",    "not.used",
-                                        "energy.MeV.u",        "number.of.energy.bins",    "EREF",            "number.of.particles.per.bin",    "cum.number"),
-                        format    =    c(    "String",            "String",                "String",            "String",                    "String",
-                                        "double",            "double",                "double",            "ulong",                    "double",
-                                        "double",            "ulong",                "2double2long",        "double",                    "ulong",
-                                        "ulong",            "double",                "ulong",            "double",                    "double"))
-
-
-    SPC.format.lookup    <-    function(SPC.table, tag){
-        return(as.character(SPC.table$format[match(tag,SPC.table$tag)]))
-    }
-
-
-    SPC.TRiP.name.lookup    <-    function(SPC.table, tag){
-        return(as.character(SPC.table$TRiP.name[match(tag,SPC.table$tag)]))
-    }
-
-    SPC.real.name.lookup    <-    function(SPC.table, tag){
-        return(as.character(SPC.table$real.name[match(tag,SPC.table$tag)]))
-    }
-
-    # 6. function for reading data items
-    read.item    <-    function(    file.handle, endian){
-        # read tag first
-        item              <-    read.tag(    file.handle, endian)
-        item$format       <-    SPC.format.lookup(    SPC.tags, item$tag)
-        item$TRiP.name    <-    SPC.TRiP.name.lookup(    SPC.tags, item$tag)
-        item$real.name    <-    SPC.real.name.lookup(    SPC.tags, item$tag)
-
-        # branch according to tag
-        if(item$format == "String"){
-            item$data <- rawToChar( readBin( file.handle, raw(), n = item$length, endian = endian))    
-        }
-
-        if(item$format == "double"){
-            item$data <- readBin( file.handle, double(), size = 8, n = floor(item$length/8), endian = endian)    
-        }
-
-        if(item$format == "2double2long"){
-            Z         <- readBin( file.handle, double(),  size = 8, n = 1, endian = endian)
-            A         <- readBin( file.handle, double(),  size = 8, n = 1, endian = endian)
-            lZ        <- readBin( file.handle, integer(), size = 4, signed = TRUE, n = 1, endian = endian)
-            lA        <- readBin( file.handle, integer(), size = 4, signed = TRUE, n = 1, endian = endian)
-            item$data <- list( Z  = Z,
-                               A  = A,
-                               lZ = lZ,
-                               lA = lA)
-        }
-
-        if(item$format == "ulong"){
-            item$data    <-    readBin(    file.handle, integer(), size = 8, signed = FALSE, n = floor(item$length/8), endian = endian)    
-        }
-        return(item)
-    }
-
-    #######################
-    # START ACTUAL FUNCTION
-    #######################
-
-    # Open file for binary access
     to.read                 <- file(file.name, "rb")
-    # read file header
-    spc                     <- list()
-    spc$file.type           <- read.item( to.read, endian)$data
-    spc$file.version        <- read.item( to.read, endian)$data
-    spc$file.date           <- read.item( to.read, endian)$data
-    spc$target.name         <- read.item( to.read, endian)$data
-    spc$projectile.name     <- read.item( to.read, endian)$data
-    spc$beam.energy.MeV.u   <- read.item( to.read, endian)$data
-    spc$peak.position.g.cm2 <- read.item( to.read, endian)$data
-    spc$normalization       <- read.item( to.read, endian)$data
-    spc$n.depth.steps       <- read.item( to.read, endian)$data
 
-    spc$data                <- NULL
-
-    for (i in 1:spc$n.depth.steps){
-        #i<-1
-        depth.g.cm2             <-    read.item( to.read, endian)$data
-        normalization           <-    read.item( to.read, endian)$data
-        n.particle.species      <-    read.item( to.read, endian)$data
-        
-        for (j in 1:n.particle.species){
-            #j<-1
-            #j<-3
-            Z.and.A                 <-    read.item( to.read, endian)$data
-            Cum                     <-    read.item( to.read, endian)$data
-            NC                      <-    read.item( to.read, endian)$data
-            n.energy.bins           <-    read.item( to.read, endian)$data
-            
-            tmp.data                <-    data.frame(   depth.step          =    rep(i,                     n.energy.bins),
-                                                        depth.g.cm2         =    rep(depth.g.cm2,           n.energy.bins),
-                                                        n.particle.species  =    rep(n.particle.species,    n.energy.bins),
-                                                        particle.species    =    rep(j,                     n.energy.bins),
-                                                        Z                   =    rep(Z.and.A$Z,             n.energy.bins),
-                                                        A                   =    rep(Z.and.A$A,             n.energy.bins),
-                                                        lZ                  =    rep(Z.and.A$lZ,            n.energy.bins),
-                                                        lA                  =    rep(Z.and.A$lA,            n.energy.bins),
-                                                        Cum                 =    rep(Cum,                   n.energy.bins),
-                                                        n.energy.bins       =    rep(n.energy.bins,         n.energy.bins),
-                                                        E.low.MeV.u         =    rep(0.0,                   n.energy.bins),
-                                                        E.mid.MeV.u         =    rep(0.0,                   n.energy.bins),
-                                                        E.high.MeV.u        =    rep(0.0,                   n.energy.bins),
-                                                        H                   =    rep(0.0,                   n.energy.bins),
-                                                        HCum                =    rep(0.0,                   n.energy.bins))
-                                        
-            tmp.item                <-    read.item( to.read, endian)
-
-            if(tmp.item$tag == 18){    #EREFCOPY        
-                ref.species             <-    tmp.item$data
-                ii                      <-    spc$data$depth.step == i & spc$data$particle.species == (ref.species + 1)
-                tmp.data$E.low.MeV.u    <-    spc$data$E.low.MeV.u[ii]
-                tmp.data$E.high.MeV.u   <-    spc$data$E.high.MeV.u[ii]
-                tmp.data$E.mid.MeV.u    <-    spc$data$E.mid.MeV.u[ii]
-            }else{
-                E.bins.MeV.u            <-    tmp.item$data
-                tmp.data$E.low.MeV.u    <-    E.bins.MeV.u[-length(E.bins.MeV.u)]
-                tmp.data$E.high.MeV.u   <-    E.bins.MeV.u[-1]
-                # calculate E.mid.MeV.u 
-                if(mean == 0){
-                    tmp.data$E.mid.MeV.u    <-    sqrt(tmp.data$E.low.MeV.u * tmp.data$E.high.MeV.u)
-                }
-                if(mean == 1){
-                    tmp.data$E.mid.MeV.u    <-    (tmp.data$E.low.MeV.u + tmp.data$E.high.MeV.u)/2
-                }            
-            }
-
-            tmp.data$H              <-    read.item( to.read, endian)$data
-            tmp.data$HCum           <-    read.item( to.read, endian)$data[-1]
-            
-            if(is.null(spc$data)){
-                spc$data                <-    tmp.data
-            }else{
-                spc$data                <-    rbind.data.frame(spc$data,tmp.data)
-            }
-            #cat(paste("Read particle species", j, "of", n.particle.species, "(Z = ", Z.and.A$lZ, "A = ", Z.and.A$lA, ")\n"))
-        }
-        cat(paste("Read depth step", i, "of", spc$n.depth.steps, "\n"))
-    }
-
-    # close file
-    close(to.read)
-
-    # add bin width
-    spc$data$DE.MeV.u    <- spc$data$E.high.MeV.u - spc$data$E.low.MeV.u
-
-    # Remove zero fluence bins if requested
-    if(compress){
-      ii         <-  spc$data$H != 0
-      spc$data   <-  spc$data[ii,]
-    }
-
-    # reformat if not requested otherwise
-    if(!raw){
-       spc$data$particle.no <- AT.particle.no.from.Z.and.A( Z = spc$data$Z,
-                                                            A = spc$data$A)$particle.no
-       spc$data$Z                  <- NULL
-       spc$data$A                  <- NULL
-       spc$data$lZ                 <- NULL
-       spc$data$lA                 <- NULL
-       spc$data$n.particle.species <- NULL
-       spc$data$particle.species   <- NULL
-       spc$data$Cum                <- NULL
-       spc$data$n.energy.bins      <- NULL
-       spc$data$E.low.MeV.u        <- NULL
-       spc$data$E.high.MeV.u       <- NULL
-       spc$data$HCum               <- NULL
-
-       spc$data$E.MeV.u            <- spc$data$E.mid.MeV.u
-       spc$data$E.mid.MeV.u        <- NULL
-
-       spc$data$fluence.cm2        <- spc$data$H * spc$data$DE.MeV.u
-       spc$data$H                  <- NULL
-
-       if(spc$target.name == "H2O"){
-           spc$target.name             <- "Water, Liquid"
+    # Scan file first to get bin sizes
+    seek(to.read, where = 0, origin = 'end')  
+    end.pos                 <- seek(to.read, where = NA)  
+    seek(to.read, where = 0, origin = 'start')
+    tags                    <- 1
+    max.tags                <- 20000
+    mtag                    <- matrix( data = 0, ncol = 4, nrow = max.tags)
+    repeat{
+       mtag[tags, 1]  <- seek(to.read, where = NA)  
+       mtag[tags, 2]  <- readBin(to.read, integer(), endian = endian)
+       if(mtag[tags, 2] > 20){
+          cat("Strange data read. Probably wrong endianess given.\n")
+          close(to.read)
+          stop()
+       }
+       mtag[tags, 3]  <- readBin(to.read, integer(), endian = endian)
+       if( mtag[tags, 2] %in% c(9,12,16,18)){
+           mtag[tags, 4]  <- readBin(    to.read, integer(), size = 8, signed = FALSE, n = 1, endian = endian)
+       }else if(mtag[tags, 2] == 10){
+           mtag[tags, 4]  <- readBin(    to.read, double(), size = 8, n = 1, endian = endian)
+       }else if(mtag[tags, 2] == 13){
+           seek(to.read, where = 16, origin = 'current')
+           mtag[tags, 4]  <- 1000 * readBin( to.read, integer(), size = 4, signed = TRUE, n = 1, endian = endian) + readBin( to.read, integer(), size = 4, signed = TRUE, n = 1, endian = endian)
+       } else{
+           seek(to.read, where = mtag[tags, 3], origin = 'current')
+       }
+       tags <- tags + 1
+       if(tags >= max.tags){
+          cat("Exceeded maximum number of tags. Please choose higher number.\n")
+          close(to.read)
+          stop()
+       }
+       if(seek(to.read, where = NA) >= end.pos){
+           break()
        }
     }
 
-    return(spc)
+    mtag               <- mtag[mtag[,1] != 0,]
+    n.depth.steps      <- mtag[mtag[,2] == 9, 4]
+    depth.g.cm2        <- mtag[mtag[,2] == 10, 4]
+    n.particle.species <- mtag[mtag[,2] == 12, 4]
+    ref.species        <- mtag[mtag[,2] == 18, 4]
+    particle.no        <- mtag[mtag[,2] == 13, 4]
+    bins               <- mtag[mtag[,2] == 16, 4]
+
+    seek(to.read, where = mtag[mtag[,2] == 5,1] + 8, origin = 'start')
+    projectile         <- rawToChar(readBin(	to.read, raw(), n = mtag[mtag[,2] == 5,3], endian = endian))	
+    seek(to.read, where = mtag[mtag[,2] == 4,1] + 8, origin = 'start')
+    target.material    <- rawToChar(readBin(	to.read, raw(), n = mtag[mtag[,2] == 4,3], endian = endian))	
+    seek(to.read, where = mtag[mtag[,2] == 6,1] + 8, origin = 'start')
+    beam.energy.MeV.u  <- readBin( to.read, double(), size = 8, n = floor(mtag[mtag[,2] == 6,3]/8), endian = endian)
+    seek(to.read, where = mtag[mtag[,2] == 7,1] + 8, origin = 'start')
+    peak.position.g.cm2<- readBin( to.read, double(), size = 8, n = floor(mtag[mtag[,2] == 7,3]/8), endian = endian)
+	
+    mm                 <- matrix(data = 0, ncol = 7, nrow = sum(bins))
+    mm[,1]             <- rep.int(rep.int(1:n.depth.steps, n.particle.species), bins)
+    mm[,2]             <- rep.int(rep.int(depth.g.cm2, n.particle.species), bins)
+    mm[,3]             <- rep.int(sequence(n.particle.species), bins)
+    mm[,4]             <- rep.int(particle.no, bins)
+
+    fluence.tags       <- mtag[mtag[,2] == 19,]
+    idx                <- 1
+    for(i in 1:nrow(fluence.tags)){
+        # i <- 1
+        seek(to.read, where = fluence.tags[i,1] + 8, origin = 'start')
+        size                   <- floor(fluence.tags[i,3]/8)
+        mm[idx:(idx+size-1),7] <- readBin( to.read, double(), size = 8, n = size, endian = endian)
+        idx                    <- idx + size
+    }
+
+    E.grid.tags        <- cbind( mtag[mtag[,2] %in% c(17,18),], 
+                                 rep.int(1:n.depth.steps, n.particle.species),   # depth step
+                                 sequence(n.particle.species),                   # species
+                                 cumsum(bins)-bins[1]+1,                         # index in mm
+                                 bins)                                           # size
+    idx                <- 1
+    for(i in 1:nrow(E.grid.tags)){
+        # i <- 2
+        if(E.grid.tags[i,2] == 17){
+            seek(to.read, where = E.grid.tags[i,1] + 8, origin = 'start')
+            size                   <- floor(E.grid.tags[i,3]/8)
+            E.bins.MeV.u           <- readBin( to.read, double(), size = 8, n = size, endian = endian)
+            E.low.MeV.u            <- E.bins.MeV.u[-length(E.bins.MeV.u)]
+            E.high.MeV.u           <- E.bins.MeV.u[-1]
+            mm[idx:(idx+size-2),6] <- E.high.MeV.u - E.low.MeV.u
+            if(mean == "geometric"){  
+                mm[idx:(idx+size-2),5]  <- sqrt(E.low.MeV.u * E.high.MeV.u)
+            }else{
+                mm[idx:(idx+size-2),5]  <- (E.low.MeV.u + E.high.MeV.u)/2
+            }            
+            idx                    <- idx + size - 1
+        }else{
+            ii                      <- E.grid.tags[,5] == E.grid.tags[i,5]
+            ref.idx                 <- which((E.grid.tags[i,4]+1) == E.grid.tags[ii,6])
+            size                    <- E.grid.tags[ii,8][ref.idx]
+            from                    <- E.grid.tags[ii,7][ref.idx]
+            mm[idx:(idx+size-1),5]  <- mm[from:(from+size-1),5]
+            mm[idx:(idx+size-1),6]  <- mm[from:(from+size-1),6]
+            idx                     <- idx + size
+        }
+    }
+    close(to.read)
+
+    # Remove zero fluence bins if requested
+    if(compress == TRUE){
+      ii         <- mm[,7] != 0
+      mm         <- mm[ii,]
+    }
+    mm           <- mm[,-3]
+    mm[,6]       <- mm[,6] * mm[,5]         # convert fluence / binwidth -> fluence
+    df           <- as.data.frame(mm)
+    names(df)    <- c("depth.step", "depth.g.cm2", "particle.no", "E.MeV.u", "DE.MeV.u", "fluence.cm2")
+
+    cat(paste("Read ", n.depth.steps, " depth steps for projectile ", projectile, " on ", target.material, " with ", beam.energy.MeV.u, " MeV/u and peak at ", peak.position.g.cm2, " g/cm2.\n", sep = ""))
+	
+    return(df)
 }
 
