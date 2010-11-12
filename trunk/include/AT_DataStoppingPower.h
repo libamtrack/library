2f85f896d1add0e1a1467a587115c1c7add42d6b
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-11-12 17:35:20 +0000

    New Stopping Power access routines, with data for PSTAR and H20 only

--- trunk/include/AT_DataStoppingPower.h
+++ trunk/include/AT_DataStoppingPower.h
@@ -41,30 +41,81 @@
  */
 enum stoppingPowerSource_no{
   PSTAR                = 0, /**< PSTAR */
-  Bethe                = 1  /**< Bethe Bloch */
+  Bethe                = 1  /**< Bethe */
 };
 
+
+/**
+ * TODO
+ */
 #define STOPPING_POWER_SOURCE_N    2
 
+
+/**
+ * TODO
+ */
+#define STOPPING_POWER_SOURCE_NAME_LENGTH    255
 
 /**
  * @struct AT_table_of_stopping_power_sources
  * TODO
  */
 typedef struct {
-  const long    n;
-  const long    source_no[STOPPING_POWER_SOURCE_N];
-  const char*   source_name[STOPPING_POWER_SOURCE_N];
-} AT_table_of_stopping_power_sources;
+  const long    n;                                                     /**< number of sources */
+  const long    stopping_power_source_no[STOPPING_POWER_SOURCE_N];     /**< TODO */
+  const char*   stopping_power_source_name[STOPPING_POWER_SOURCE_N];   /**< TODO */
+} AT_stopping_power_sources_struct;
 
 
-static const AT_table_of_stopping_power_sources AT_Stopping_Power_Sources = {
-	STOPPING_POWER_SOURCE_N,
-    {  PSTAR,                  Bethe                    }, // source_no
-    {  "PSTAR data ... TODO",  "Bethe formula ... TODO "}  // source_name
-};
+/**
+ * @struct AT_stopping_power_tabulated_source
+ * TODO
+ */
+typedef struct {
+  const long       number_of_data_points;                              /**< number of data points for given material and source */
+  const long       stopping_power_source_no;
+  const long       material_no;
+  const double     E_MeV_u_and_stopping_power_total_MeV_cm2_g[][2];
+} AT_stopping_power_tabulated_source_for_given_material_struct;
 
 
+/**
+ * @struct AT_stopping_power_tabulated_source_group_for_all_materials_struct
+ * TODO
+ */
+typedef struct {
+  const long                                                            number_of_materials;            /**< number of data points for given source */
+  const long                                                            stopping_power_source_no;
+  const long                                                            material_no[MATERIAL_DATA_N];
+  const AT_stopping_power_tabulated_source_for_given_material_struct *  stopping_power_source_data[];
+} AT_stopping_power_tabulated_source_group_for_all_materials_struct;
+
+
+/**
+ * @struct AT_stopping_power_tabulated_source_group_struct
+ * TODO
+ */
+typedef struct {
+  const long                                                                 number_of_sources;         /**< number of data sources */
+  const long                                                                 stopping_power_source_no[STOPPING_POWER_SOURCE_N];
+  const AT_stopping_power_tabulated_source_group_for_all_materials_struct *  stopping_power_source_data_group[];
+} AT_stopping_power_tabulated_source_group_struct;
+
+
+typedef double (*pointer_to_stopping_power_analytical_function)(const double, const long, const long);
+
+/**
+ * @struct AT_stopping_power_analytical_sources_struct
+ * TODO
+ */
+typedef struct {
+  const long                                     n;
+  const long                                     stopping_power_source_no[STOPPING_POWER_SOURCE_N];
+  pointer_to_stopping_power_analytical_function  access_function[STOPPING_POWER_SOURCE_N];
+} AT_stopping_power_analytical_sources_struct;
+
+
+/** ----------------------------------------------- FUNCTIONS --------------------------------------------- */
 
 /**
  * TODO
@@ -79,8 +130,95 @@ void AT_stopping_power_source_model_name_from_number( const long source_no, char
  * @param[in] source_name
  * @return    source number
  */
-long AT_stopping_power_source_model_number_from_name( const char* material_name );
+long AT_stopping_power_source_model_number_from_name( const char* source_name );
 
+
+/**
+ * Wrapper for Bethe analytical function
+ * @param[in] E_MeV_u
+ * @param[in] particle_no
+ * @param[in] material_no
+ * @return
+ */
+double AT_Bethe_wrapper(const double 	E_MeV_u,
+		const long 	    particle_no,
+		const long 		material_no);
+
+
+/**
+ * Interpolation over tabulated Stopping Power data
+ * @param[in] stopping_power_source_no
+ * @param[in] E_MeV_u
+ * @param[in] particle_no
+ * @param[in] material_no
+ * @return    gives stopping power for given energy
+ */
+double AT_Stopping_Power_data_interpolation(const long stopping_power_source_no,
+		const double 	E_MeV_u,
+		const long 	    particle_no,
+		const long 		material_no);
+
+
+/**
+ * Main access method to stopping power data
+ * @param[in] stopping_power_source_no
+ * @param[in] E_MeV_u
+ * @param[in] particle_no
+ * @param[in] material_no
+ * @return stopping power
+ */
+double AT_Stopping_Power_MeV_cm2_g_single( const long stopping_power_source_no,
+		const double E_MeV_u,
+		const long particle_no,
+		const long material_no);
+
+
+/**
+ * Main access method to stopping power data - multiple field
+ * @param[in] stopping_power_source_no
+ * @param[in] number_of_particles
+ * @param[in] E_MeV_u (array of size number_of_particles)
+ * @param[in] particle_no (array of size number_of_particles)
+ * @param[in] material_no
+ * @param[out] Stopping_Power_MeV_cm2_g (array of size number_of_particles)
+ */
+void AT_Stopping_Power_MeV_cm2_g_multi( const long stopping_power_source_no,
+		const long number_of_particles,
+		const double E_MeV_u[],
+		const long particle_no[],
+		const long material_no,
+		double Stopping_Power_MeV_cm2_g[]);
+
+
+/**
+ * Main access method to stopping power data
+ * @param[in] stopping_power_source_no
+ * @param[in] E_MeV_u
+ * @param[in] particle_no
+ * @param[in] material_no
+ * @return stopping power
+ */
+double AT_Stopping_Power_keV_um_single( const long stopping_power_source_no,
+		const double E_MeV_u,
+		const long particle_no,
+		const long material_no);
+
+
+/**
+ * Main access method to stopping power data - multiple field
+ * @param[in] stopping_power_source_no
+ * @param[in] number_of_particles
+ * @param[in] E_MeV_u (array of size number_of_particles)
+ * @param[in] particle_no (array of size number_of_particles)
+ * @param[in] material_no
+ * @param[out] Stopping_Power_keV_um (array of size number_of_particles)
+ */
+void AT_Stopping_Power_keV_um_multi( const long stopping_power_source_no,
+		const long number_of_particles,
+		const double E_MeV_u[],
+		const long particle_no[],
+		const long material_no,
+		double Stopping_Power_keV_um[]);
 
 
 /**
@@ -95,10 +233,11 @@ long AT_stopping_power_source_model_number_from_name( const char* material_name 
  * @param[in]      E_restricted_keV 	if positive and smaller than maximally transferable energy, the restricted stopping number will be computed
  * @return     result
  */
-double AT_Bethe_Stopping_Number_single(	const double 	E_MeV_u,
-										const long 	particle_no,
+double AT_Stopping_Power_Bethe_Number(	const double 	E_MeV_u,
+										const long      particle_no,
 										const long 		material_no,
 										const double	E_restricted_keV);
+
 
 /**
  * Computes the mass stopping power using the Bethe formula
@@ -112,10 +251,11 @@ double AT_Bethe_Stopping_Number_single(	const double 	E_MeV_u,
  * @param[in]      E_restricted_keV 	if positive and smaller than maximally transferable energy, the restricted stopping power will be computed
  * @return     result
  */
-double AT_Bethe_Mass_Stopping_Power_MeV_cm2_g_single(	const double 	E_MeV_u,
-														const long 	particle_no,
+double AT_Stopping_Power_Mass_Bethe_MeV_cm2_g_single(	const double 	E_MeV_u,
+														const long 	    particle_no,
 														const long 		material_no,
 														const double	E_restricted_keV);
+
 
 /**
  * Computes the mass stopping power using the Bethe formula
@@ -130,12 +270,185 @@ double AT_Bethe_Mass_Stopping_Power_MeV_cm2_g_single(	const double 	E_MeV_u,
  * @param[in]      E_restricted_keV 	if positive and smaller than maximally transferable energy, the restricted stopping power will be computed (single value)
  * @param[out]     Mass_Stopping_Power_MeV_cm2_g (array of size n)
  */
-void AT_Bethe_Mass_Stopping_Power_MeV_cm2_g(	const long n,
+void AT_Stopping_Power_Mass_Bethe_MeV_cm2_g_multi(	const long n,
 		const double E_MeV_u[],
 		const long particle_no[],
 		const long material_no,
 		const double E_restricted_keV,
 		double Mass_Stopping_Power_MeV_cm2_g[]);
 
+
+/** ----------------------------------------------- PSTAR DATA --------------------------------------------- */
+static const AT_stopping_power_tabulated_source_for_given_material_struct AT_stopping_power_data_PSTAR_Water = {
+  132,
+  PSTAR,
+  Water_Liquid,
+  {
+		  { 1.000E-03 , 1.769E+02 },
+		  { 1.500E-03 , 1.984E+02 },
+		  { 2.000E-03 , 2.184E+02 },
+		  { 2.500E-03 , 2.370E+02 },
+		  { 3.000E-03 , 2.544E+02 },
+		  { 4.000E-03 , 2.864E+02 },
+		  { 5.000E-03 , 3.153E+02 },
+		  { 6.000E-03 , 3.420E+02 },
+		  { 7.000E-03 , 3.667E+02 },
+		  { 8.000E-03 , 3.900E+02 },
+		  { 9.000E-03 , 4.120E+02 },
+		  { 1.000E-02 , 4.329E+02 },
+		  { 1.250E-02 , 4.745E+02 },
+		  { 1.500E-02 , 5.110E+02 },
+		  { 1.750E-02 , 5.437E+02 },
+		  { 2.000E-02 , 5.733E+02 },
+		  { 2.250E-02 , 6.001E+02 },
+		  { 2.500E-02 , 6.245E+02 },
+		  { 2.750E-02 , 6.467E+02 },
+		  { 3.000E-02 , 6.671E+02 },
+		  { 3.500E-02 , 7.028E+02 },
+		  { 4.000E-02 , 7.324E+02 },
+		  { 4.500E-02 , 7.569E+02 },
+		  { 5.000E-02 , 7.768E+02 },
+		  { 5.500E-02 , 7.927E+02 },
+		  { 6.000E-02 , 8.050E+02 },
+		  { 6.500E-02 , 8.142E+02 },
+		  { 7.000E-02 , 8.205E+02 },
+		  { 7.500E-02 , 8.243E+02 },
+		  { 8.000E-02 , 8.260E+02 },
+		  { 8.500E-02 , 8.258E+02 },
+		  { 9.000E-02 , 8.239E+02 },
+		  { 9.500E-02 , 8.206E+02 },
+		  { 1.000E-01 , 8.161E+02 },
+		  { 1.250E-01 , 7.814E+02 },
+		  { 1.500E-01 , 7.371E+02 },
+		  { 1.750E-01 , 6.969E+02 },
+		  { 2.000E-01 , 6.613E+02 },
+		  { 2.250E-01 , 6.294E+02 },
+		  { 2.500E-01 , 6.006E+02 },
+		  { 2.750E-01 , 5.744E+02 },
+		  { 3.000E-01 , 5.504E+02 },
+		  { 3.500E-01 , 5.080E+02 },
+		  { 4.000E-01 , 4.719E+02 },
+		  { 4.500E-01 , 4.406E+02 },
+		  { 5.000E-01 , 4.132E+02 },
+		  { 5.500E-01 , 3.891E+02 },
+		  { 6.000E-01 , 3.680E+02 },
+		  { 6.500E-01 , 3.492E+02 },
+		  { 7.000E-01 , 3.325E+02 },
+		  { 7.500E-01 , 3.175E+02 },
+		  { 8.000E-01 , 3.039E+02 },
+		  { 8.500E-01 , 2.917E+02 },
+		  { 9.000E-01 , 2.805E+02 },
+		  { 9.500E-01 , 2.702E+02 },
+		  { 1.000E+00 , 2.608E+02 },
+		  { 1.250E+00 , 2.229E+02 },
+		  { 1.500E+00 , 1.957E+02 },
+		  { 1.750E+00 , 1.749E+02 },
+		  { 2.000E+00 , 1.586E+02 },
+		  { 2.250E+00 , 1.454E+02 },
+		  { 2.500E+00 , 1.344E+02 },
+		  { 2.750E+00 , 1.251E+02 },
+		  { 3.000E+00 , 1.172E+02 },
+		  { 3.500E+00 , 1.042E+02 },
+		  { 4.000E+00 , 9.404E+01 },
+		  { 4.500E+00 , 8.586E+01 },
+		  { 5.000E+00 , 7.911E+01 },
+		  { 5.500E+00 , 7.343E+01 },
+		  { 6.000E+00 , 6.858E+01 },
+		  { 6.500E+00 , 6.438E+01 },
+		  { 7.000E+00 , 6.071E+01 },
+		  { 7.500E+00 , 5.747E+01 },
+		  { 8.000E+00 , 5.460E+01 },
+		  { 8.500E+00 , 5.202E+01 },
+		  { 9.000E+00 , 4.969E+01 },
+		  { 9.500E+00 , 4.759E+01 },
+		  { 1.000E+01 , 4.567E+01 },
+		  { 1.250E+01 , 3.815E+01 },
+		  { 1.500E+01 , 3.292E+01 },
+		  { 1.750E+01 , 2.905E+01 },
+		  { 2.000E+01 , 2.607E+01 },
+		  { 2.500E+01 , 2.175E+01 },
+		  { 2.750E+01 , 2.013E+01 },
+		  { 3.000E+01 , 1.876E+01 },
+		  { 3.500E+01 , 1.656E+01 },
+		  { 4.000E+01 , 1.488E+01 },
+		  { 4.500E+01 , 1.354E+01 },
+		  { 5.000E+01 , 1.245E+01 },
+		  { 5.500E+01 , 1.154E+01 },
+		  { 6.000E+01 , 1.078E+01 },
+		  { 6.500E+01 , 1.013E+01 },
+		  { 7.000E+01 , 9.559E+00 },
+		  { 7.500E+01 , 9.063E+00 },
+		  { 8.000E+01 , 8.625E+00 },
+		  { 8.500E+01 , 8.236E+00 },
+		  { 9.000E+01 , 7.888E+00 },
+		  { 9.500E+01 , 7.573E+00 },
+		  { 1.000E+02 , 7.289E+00 },
+		  { 1.250E+02 , 6.192E+00 },
+		  { 1.500E+02 , 5.445E+00 },
+		  { 1.750E+02 , 4.903E+00 },
+		  { 2.000E+02 , 4.492E+00 },
+		  { 2.250E+02 , 4.170E+00 },
+		  { 2.500E+02 , 3.911E+00 },
+		  { 2.750E+02 , 3.698E+00 },
+		  { 3.000E+02 , 3.520E+00 },
+		  { 3.500E+02 , 3.241E+00 },
+		  { 4.000E+02 , 3.032E+00 },
+		  { 4.500E+02 , 2.871E+00 },
+		  { 5.000E+02 , 2.743E+00 },
+		  { 5.500E+02 , 2.640E+00 },
+		  { 6.000E+02 , 2.556E+00 },
+		  { 6.500E+02 , 2.485E+00 },
+		  { 7.000E+02 , 2.426E+00 },
+		  { 7.500E+02 , 2.376E+00 },
+		  { 8.000E+02 , 2.333E+00 },
+		  { 8.500E+02 , 2.296E+00 },
+		  { 9.000E+02 , 2.264E+00 },
+		  { 9.500E+02 , 2.236E+00 },
+		  { 1.000E+03 , 2.211E+00 },
+		  { 1.500E+03 , 2.070E+00 },
+		  { 2.000E+03 , 2.021E+00 },
+		  { 2.500E+03 , 2.004E+00 },
+		  { 3.000E+03 , 2.001E+00 },
+		  { 4.000E+03 , 2.012E+00 },
+		  { 5.000E+03 , 2.031E+00 },
+		  { 6.000E+03 , 2.052E+00 },
+		  { 7.000E+03 , 2.072E+00 },
+		  { 8.000E+03 , 2.091E+00 },
+		  { 9.000E+03 , 2.109E+00 },
+		  { 1.000E+04 , 2.126E+00 }
+  }
+};
+
+
+/** ----------------------------------------------- Static objects  --------------------------------------- */
+
+
+static const AT_stopping_power_sources_struct AT_stopping_power_sources = {
+	STOPPING_POWER_SOURCE_N,
+    {  PSTAR,         Bethe           }, // source_no
+    {  "PSTAR data",  "Bethe formula" }  // source_name
+};
+
+
+static const AT_stopping_power_tabulated_source_group_for_all_materials_struct AT_data_PSTAR_source = {
+  MATERIAL_DATA_N,
+  PSTAR,
+  { User_Defined_Material,  Water_Liquid,                         Aluminum_Oxide, Aluminum, PMMA, Alanine, LiF,  Air },
+  { NULL,                   &AT_stopping_power_data_PSTAR_Water,  NULL,           NULL,     NULL, NULL,    NULL, NULL}
+};
+
+
+static const AT_stopping_power_tabulated_source_group_struct AT_stopping_power_tabulated_source = {
+  STOPPING_POWER_SOURCE_N,
+  { PSTAR,                  Bethe},
+  { &AT_data_PSTAR_source,  NULL}
+};
+
+
+static const AT_stopping_power_analytical_sources_struct AT_stopping_power_analytical_source = {
+  STOPPING_POWER_SOURCE_N,
+  { PSTAR,   Bethe},
+  { NULL,    &AT_Bethe_wrapper}
+};
 
 #endif /* AT_DATASTOPPINGPOWER_H_ */
