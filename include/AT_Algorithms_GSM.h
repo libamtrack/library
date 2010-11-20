1e98f40c7e070fb4b64319a74b481dd886ffea99
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-11-20 17:32:14 +0000

    (Hopefully) More describing function names for GSM

--- include/AT_Algorithms_GSM.h
+++ include/AT_Algorithms_GSM.h
@@ -67,7 +67,7 @@
  *                                                         2nd dimension - particle index for given component)
  *                                                         of Y-positions of particles of each type in the mixed particle field
  */
-void AT_GSM_shoot_particles_on_grid( const long  number_of_field_components,
+void AT_GSM_sample_particle_positions( const long  number_of_field_components,
     const double         fluence_cm2[],
     const double         sample_grid_size_m,
     unsigned long* 		 random_number_generator_seed,
@@ -101,7 +101,7 @@ void AT_GSM_shoot_particles_on_grid( const long  number_of_field_components,
  * @param[in]  pixel_size_m                              linear size of pixel in grid
  * @param[out] grid_D_Gy                                 grid dose pattern (2-D array of dimensions nX x nX, linearly allocated)
  */
-void AT_GSM_calculate_dose_pattern( const long  number_of_field_components,
+void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_components,
     const double   E_MeV_u[],
     const long     particle_no[],
     const long     material_no,
@@ -126,45 +126,13 @@ void AT_GSM_calculate_dose_pattern( const long  number_of_field_components,
  * @param[out]  frequency          frequency vector for resulting histogram
  * @length number_of_bins
  */
-void AT_GSM_calculate_histogram_from_grid( const long     nX,
+void AT_GSM_local_dose_distrib_from_dose_grid( const long     nX,
     const double** grid,
     const long     number_of_bins,
     const double   bin_centers_Gy[],
     double *       zero_fraction,
     double         frequency[]);
 
-
-/**
- * Computes local dose histogram for given mixed field
- *
- * @param[in]  number_of_field_components                number of particle types in the mixed particle field
- * @param[in]  E_MeV_u                                   energy of particles in the mixed particle field (array of size number_of_field_components)
- * @param[in]  fluence_cm2                               fluences for the given particles (array of size number_of_field_components)
- * @param[in]  particle_no                               type of the particles in the mixed particle field (array of size number_of_field_components)
- * @see          AT_DataParticle.h for definition
- * @param[in]  material_no                               index number for detector material
- * @see          AT_DataMaterial.h for definition
- * @param[in]  rdd_model                                 index number for chosen radial dose distribution
- * @param[in]  rdd_parameter                             parameters for chosen radial dose distribution (array of size depending on chosen model)
- * @see          AT_RDD.h for definition
- * @param[in]  er_model                                  index number for chosen electron-range model
- * @see          AT_ElectronRange.h for definition
- */
-void AT_GSM_calculate_dose_histogram( const long  number_of_field_components,
-    const double   E_MeV_u[],
-    const double   fluence_cm2[],
-    const long     particle_no[],
-    const long     material_no,
-    const long     rdd_model,
-    const double   rdd_parameter[],
-    const long     er_model,
-    const long     nX,
-    const double   pixel_size_m,
-    const long     number_of_bins,
-    const double   dose_bin_centers_Gy[],
-    unsigned long* random_number_generator_seed,
-    double *       zero_dose_fraction,
-    double         dose_frequency_Gy[]);
 
 
 /**
@@ -178,58 +146,12 @@ void AT_GSM_calculate_dose_histogram( const long  number_of_field_components,
  * @param[in]  lethal_events_mode                        if true, allows to do calculations for cell survival
  * @param[out] grid_response                             response pattern (array of size nX*nX)
  */
-void AT_GSM_calculate_local_response_grid( const long      nX,
+void AT_GSM_response_grid_from_dose_grid( const long      nX,
                 const long      gamma_model,
                 const double    gamma_parameters[],
                 const double**  grid_D_Gy,
                 const bool      lethal_events_mode,
                 double**        grid_response);
-
-/**
- * TODO
- * @param number_of_field_components
- * @param E_MeV_u
- * @param fluence_cm2
- * @param particle_no
- * @param material_no
- * @param rdd_model
- * @param rdd_parameter
- * @param er_model
- * @param nX
- * @param pixel_size_m
- * @param N_runs
- * @param N_repetitions
- * @param number_of_bins
- * @param dose_bin_centers_Gy
- * @param dose_bin_width_Gy
- * @param mean_d_check_Gy
- * @param sd_d_check_Gy
- * @param mean_zero_dose_fraction
- * @param sd_zero_dose_fraction
- * @param mean_dose_frequency_Gy
- * @param sd_dose_frequency_Gy
- */
-void AT_GSM_calculate_multiple_dose_histograms( const long  number_of_field_components,
-    const double   	E_MeV_u[],
-    const double   	fluence_cm2[],
-    const long     	particle_no[],
-    const long     	material_no,
-    const long     	rdd_model,
-    const double   	rdd_parameter[],
-    const long     	er_model,
-    const long     	nX,
-    const double   	pixel_size_m,
-    const long		N_runs,
-    const long		N_repetitions,
-    const long     	number_of_bins,
-    const double   	dose_bin_centers_Gy[],
-    double    		dose_bin_width_Gy[],
-    double *       	mean_d_check_Gy,
-    double *       	sd_d_check_Gy,
-    double *       	mean_zero_dose_fraction,
-    double *       	sd_zero_dose_fraction,
-    double        	mean_dose_frequency_Gy[],
-    double        	sd_dose_frequency_Gy[]);
 
 /**
  * Computes HCP response and relative efficiency/RBE using summation of tracks
@@ -295,6 +217,84 @@ void AT_run_GSM_method(  const long  number_of_field_components,
     double*        sd_S_HCP,
     double*        sd_S_gamma,
     double*        sd_n_particles);
+
+/**
+ * Computes local dose histogram for given mixed field
+ *
+ * @param[in]  number_of_field_components                number of particle types in the mixed particle field
+ * @param[in]  E_MeV_u                                   energy of particles in the mixed particle field (array of size number_of_field_components)
+ * @param[in]  fluence_cm2                               fluences for the given particles (array of size number_of_field_components)
+ * @param[in]  particle_no                               type of the particles in the mixed particle field (array of size number_of_field_components)
+ * @see          AT_DataParticle.h for definition
+ * @param[in]  material_no                               index number for detector material
+ * @see          AT_DataMaterial.h for definition
+ * @param[in]  rdd_model                                 index number for chosen radial dose distribution
+ * @param[in]  rdd_parameter                             parameters for chosen radial dose distribution (array of size depending on chosen model)
+ * @see          AT_RDD.h for definition
+ * @param[in]  er_model                                  index number for chosen electron-range model
+ * @see          AT_ElectronRange.h for definition
+ */
+void AT_GSM_local_dose_distrib( const long  number_of_field_components,
+    const double   E_MeV_u[],
+    const double   fluence_cm2[],
+    const long     particle_no[],
+    const long     material_no,
+    const long     rdd_model,
+    const double   rdd_parameter[],
+    const long     er_model,
+    const long     nX,
+    const double   pixel_size_m,
+    const long     number_of_bins,
+    const double   dose_bin_centers_Gy[],
+    unsigned long* random_number_generator_seed,
+    double *       zero_dose_fraction,
+    double         dose_frequency_Gy[]);
+
+/**
+ * TODO
+ * @param number_of_field_components
+ * @param E_MeV_u
+ * @param fluence_cm2
+ * @param particle_no
+ * @param material_no
+ * @param rdd_model
+ * @param rdd_parameter
+ * @param er_model
+ * @param nX
+ * @param pixel_size_m
+ * @param N_runs
+ * @param N_repetitions
+ * @param number_of_bins
+ * @param dose_bin_centers_Gy
+ * @param dose_bin_width_Gy
+ * @param mean_d_check_Gy
+ * @param sd_d_check_Gy
+ * @param mean_zero_dose_fraction
+ * @param sd_zero_dose_fraction
+ * @param mean_dose_frequency_Gy
+ * @param sd_dose_frequency_Gy
+ */
+void AT_GSM_multiple_local_dose_distrib( const long  number_of_field_components,
+    const double   	E_MeV_u[],
+    const double   	fluence_cm2[],
+    const long     	particle_no[],
+    const long     	material_no,
+    const long     	rdd_model,
+    const double   	rdd_parameter[],
+    const long     	er_model,
+    const long     	nX,
+    const double   	pixel_size_m,
+    const long		N_runs,
+    const long		N_repetitions,
+    const long     	number_of_bins,
+    const double   	dose_bin_centers_Gy[],
+    double    		dose_bin_width_Gy[],
+    double *       	mean_d_check_Gy,
+    double *       	sd_d_check_Gy,
+    double *       	mean_zero_dose_fraction,
+    double *       	sd_zero_dose_fraction,
+    double        	mean_dose_frequency_Gy[],
+    double        	sd_dose_frequency_Gy[]);
 
 
 #endif /* AT_ALGORITHMS_GSM_H_ */
