e103e18623173ca6b70c8d41cd3445c3dceaeec2
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-12-01 09:34:08 +0000

    Added PreRun-Script to trigger build of latest libamtrack version and load it for direct access in R during run of test scripts Modified first test script to run with PreRun-Script

---
+++ test/R/AT_Test_PreRun.R
@@ -0,0 +1,33 @@
+#################################################
+# Pre-run script for R test scripts in libamtrack
+#
+# This will trigger the build of the latest
+# version of library/wrappers for direct R 
+# access and load them if necessary
+# 
+# Should be called by "source" statement from
+# corresponding test script
+#
+# Started 2010-12-01, Steffen Greilich
+#################################################
+
+# Navigate to wrapper directory
+start.dir           <- getwd()
+if(try(setwd("..\\..\\wrapper\\R\\R_direct_access")) == FALSE){
+    stop("Please start script from /test/R directory")
+}
+
+# Trigger new build
+try(system("create.direct.access.windows.bat"))
+try(system("./create.direct.access.sh"))
+
+# Load library and wrappers
+try(dyn.load("libamtrack.dll"))
+try(dyn.load("libamtrack.so"))
+try(dyn.load("libamtrack.dylib"))
+
+# Source wrappers
+source("libamtrack.R")
+
+# Return to original directory
+setwd(start.dir)
