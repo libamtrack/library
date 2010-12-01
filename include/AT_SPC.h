83aff349522e8662db8b17b715f9c5aaaf6e9855
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-12-01 09:26:21 +0000

    Added doxygen comments Added length-of-arrays variable to AT_SPC_read_data

--- include/AT_SPC.h
+++ include/AT_SPC.h
@@ -33,8 +33,11 @@
 #include <stdbool.h>
 #include <string.h>
 
+/**
+ * @enum spc tags
+ */
 enum {
-       TRPSPCBTAG_FILETYPE   =1,      /* header info */
+       TRPSPCBTAG_FILETYPE   =1,      /** < header info */
        TRPSPCBTAG_FILEVERSION=2,
        TRPSPCBTAG_FILEDATE   =3,
        TRPSPCBTAG_TARGNAME   =4,
@@ -56,19 +59,76 @@ enum {
        TRPSPCDTAG_RUNNINGSUM =20,
 };
 
+/**
+ * structure for spc tags
+ */
+
 struct STRPSPCBTAG { uint32_t ulTag; uint32_t ulLen; };
 
+/**
+ * Swaps endianess of two-byte types
+ *
+ * @param[in/out]	x	pointer to variable to be swapped
+ */
 void endian_swap2(    unsigned short* x);
+
+/**
+ * Swaps endianess of four-byte types
+ *
+ * @param[in/out]	x	pointer to variable to be swapped
+ */
 void endian_swap4(    unsigned int* x);
-// __int64 for MSVC, "long long" for gcc
+
+/**
+ * Swaps endianess of eight-byte types (uint64_t MSVC, long long in gcc)
+ *
+ * @param[in/out]	x	pointer to variable to be swapped
+ */
 void endian_swap8(     uint64_t * x);
+
+/**
+ * Reads tag into tag structure
+ *
+ * @param[in/out]	fp				pointer to spc file
+ * @param[in/out]	str				pointer to spc tag structure to be filled
+ * @param[in]		switchEndian	if TRUE endianess will be swapped
+ */
 void readStruct(      FILE *fp,
 		struct STRPSPCBTAG * str,
 		bool switchEndian);
 
+/**
+ * Browses spc file to get total number of energy bins covered.
+ * This is needed for later memory allocation when reading the
+ * actual data.
+ *
+ * @param[in/out]	fp				pointer to spc file
+ * @return							total number of bins in spc file
+ */
 int AT_SPC_get_size(  FILE *fp);
+
+/**
+ * Reads data from spc file into pre-allocated arrays. It will be converted
+ * for direct use in libamtrack, i.e. to an R-style array of six columns
+ * (each presented by a single pointer) and all of same length. That of course
+ * results in redundancy in depth_step, depth_g_cm2, particle_no but enables
+ * easy division into cells (i.e. passing all spectra of a specific depth and
+ * particle number to another routine such as total dose). Please note that
+ * the fluence IS NOT normalized to bin width but given in absolute fluence!
+ *
+ * @param[in/out]	fp				pointer to spc file
+ * @param[in]		total_n_bins	size of pre-allocated arrays
+ * @see AT_SPC_get_size
+ * @param[out]		depth_step		depth step index, zero-based (array of size total_n_bins)
+ * @param[out]		depth_g_cm2		depth in g/cm2 (array of size total_n_bins)
+ * @param[out]		E_MeV_u			midpoints of energy bins (array of size total_n_bins)
+ * @param[out]		DE_MeV_u		widths of energy bins (array of size total_n_bins)
+ * @param[out]		particle_no		particle index numbers (array of size total_n_bins)
+ * @param[out]      fluence_cm2		fluence values differential in energy and particle number
+ */
 int AT_SPC_read_data( FILE *fp,
-		long depthStep[],
+		const long total_n_bins,
+		long depth_step[],
 		double depth_g_cm2[],
 		double E_MeV_u[],
 		double DE_MeV_u[],
