914d603083fd8b8c679c967320aaea6ea1746150
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-12-02 15:13:02 +0000

    Direct R wrapper generator script for linux

---
+++ trunk/wrapper/R/R_direct_access/create.direct.access.linux.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "*************************************************"
+echo "Batch file for R wrappers compiling under Windows"
+echo "*************************************************"
+
+echo "Auto-generate wrappers and documentation"
+R --no-save < ../../../tools/automatic_wrapper_generator/collect.doxygen.information.R
+R --no-save < ../../../tools/automatic_wrapper_generator/R.generate.C.wrapper.R
+R --no-save < ../../../tools/automatic_wrapper_generator/R.generate.R.wrapper.R
+
+echo "Copy sources to local directory"
+cp -r ../../../include/*.h .
+cp -r ../../../src/*.c .
+
+echo "Compile libamtrack including R wrappers"
+make all
+
+echo "Delete temporary files"
+rm -rf *.o
+rm -rf *.c
+rm -rf *.h
+rm -rf .RData
+rm -rf functions.sdd
+rm -rf ../../../tools/automatic_wrapper_generator/*.Rout
+
+echo "Done."
