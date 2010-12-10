96071b0115b2d06e07042ae8f9a8939427a27054
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-12-10 07:36:25 +0000

    Fixed minor bugs in package documentation

--- wrapper/R/R_package_devel/hardcoded_wrappers/AT.SPC.read.R
+++ wrapper/R/R_package_devel/hardcoded_wrappers/AT.SPC.read.R
@@ -1,4 +1,4 @@
-AT.SPC.read <- function(file.name){
+AT.SPC.read <- function(file.name, mean = c("arithmetic", "geometric")[1], compress = TRUE){
      spc.size            <- numeric(1)
      res                 <- .C( "AT_SPC_get_size_from_filename_R",
                                 file.name          = as.character(file.name),
@@ -25,10 +25,14 @@ AT.SPC.read <- function(file.name){
                                 n.bins.read        = as.integer(n.bins.read),
                                 PACKAGE            = "libamtrack")
     
-    return( data.frame(  depth.step             = res$depth.step,
+    df   <- data.frame(  depth.step             = res$depth.step,
                          depth.g.cm2            = res$depth.g.cm2,
                          E.MeV.u                = res$E.MeV.u,
                          DE.MeV.u               = res$DE.MeV.u,
                          particle.no            = res$particle.no,
-                         fluence.cm2            = res$fluence.cm2))
+                         fluence.cm2            = res$fluence.cm2)
+
+    if (compress == TRUE){
+        df  <- df[df$fluence.cm2 != 0,]
+    }
 }
--- wrapper/R/R_package_devel/package/R/initial.R
+++ wrapper/R/R_package_devel/package/R/initial.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg){
  library.dynam("libamtrack", pkg, lib)
- cat("This is libamtrack 0.5.0 'Black Wombat' (devel, r785).\nType '?libamtrack' for help.\n")
+ cat("This is libamtrack 0.5.0 'Black Wombat' (devel, r805).\nType '?libamtrack' for help.\n")
 }
 
 .Last.lib <- function(libpath){
