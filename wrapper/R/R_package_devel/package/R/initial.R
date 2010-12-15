adf39b386aa887747c9920a97053261cae54a4d5
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2010-12-15 06:15:47 +0000

    Replace R package by development version, step 2: delete devel folder

--- wrapper/R/R_package_devel/package/R/initial.R
+++
@@ -1,9 +0,0 @@
-.First.lib <- function(lib, pkg){
- library.dynam("libamtrack", pkg, lib)
- cat("This is libamtrack 0.5.0 'Black Wombat' (devel, r805).\nType '?libamtrack' for help.\n")
-}
-
-.Last.lib <- function(libpath){
- try(library.dynam.unload("libamtrack", libpath))
-}
-
