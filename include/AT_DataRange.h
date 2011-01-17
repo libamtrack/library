b3d6915b4abac92d4a95e3348213a52ae336d39c
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-01-17 15:58:00 +0000

    - Added function for CSDA range using integratiion of Bethe stopping power - Added two new materials (Gammex tissue surrogates as test case for a more complete set of approx. 10) - Updated doxygen comments (AT_Z_from_particle_no etc.) for use of function in R package

--- include/AT_DataRange.h
+++ include/AT_DataRange.h
@@ -35,5 +35,49 @@
 #include "AT_Error.h"
 #include "AT_NumericalRoutines.h"
 #include "AT_PhysicsRoutines.h"
+#include "AT_DataStoppingPower.h"
+
+
 
 #endif /* AT_DATARANGE_H_ */
+
+/**
+ * Structure to hold parameters other then parameter to integrate over
+ * for GSL integration routine used in AT_CSDA_range_Bethe_cm2_g
+ */
+typedef struct {
+  long    particle_no;
+  long    material_no;
+} AT_CSDA_range_Bethe_parameters;
+
+double AT_Stopping_Power_Mass_Bethe_MeV_cm2_g_int( double  r_m,
+    void*   params);
+
+/**
+ * Computes the CSDA range using the Bethe formula (AT_Stopping_Power_Bethe_Number)
+ * according to ICRU49, p.6, Eq. 2.1
+ * BUT WITHOUT shell or density, Bloch or Barkas correction!
+ * @param[in]  	   E_MeV_u      energy of particle per nucleon
+ * @param[in]  	   particle_no  particle index
+ * @see          AT_DataParticle.h for definition
+ * @param[in]      material_no  material index
+ * @see          AT_DataMaterial.h for definition
+ * @return     result
+ */
+double AT_CSDA_range_Bethe_cm2_g(	const double 	E_MeV_u,
+							const long 		particle_no,
+							const long 		material_no);
+
+/**
+ * Computes the water equivalent path length (WEPL) using the Bethe formula
+ * @param[in]  	   E_MeV_u      energy of particle per nucleon
+ * @param[in]  	   particle_no  particle index
+ * @see          AT_DataParticle.h for definition
+ * @param[in]      material_no  material index
+ * @see          AT_DataMaterial.h for definition
+ * @return     result
+ */
+
+double AT_WEPL_Bethe(	const double 	E_MeV_u,
+		const long 		particle_no,
+		const long 		material_no);
--- src/AT_DataRange.c
+++ src/AT_DataRange.c
@@ -29,4 +29,71 @@
  */
 
 #include "AT_DataRange.h"
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_randist.h>
 
+double AT_Stopping_Power_Mass_Bethe_MeV_cm2_g_int( double  E_MeV_u,
+    void*   params){
+  assert( params != NULL );
+  AT_CSDA_range_Bethe_parameters* int_params = (AT_CSDA_range_Bethe_parameters*)params;
+
+  const double E_restricted_keV = 0.0;
+  double  StPow = AT_Stopping_Power_Mass_Bethe_MeV_cm2_g_single(	E_MeV_u,
+			int_params->particle_no,
+			int_params->material_no,
+			E_restricted_keV);
+  return (1.0 / StPow);
+}
+
+double AT_CSDA_range_Bethe_cm2_g(	const double 	E_MeV_u,
+		const long 		particle_no,
+		const long 		material_no){
+
+	double range_cm2_g              = 0.0;
+	AT_CSDA_range_Bethe_parameters  params;
+	params.material_no             = material_no;
+	params.particle_no			   = particle_no;
+
+	/* Initialize GSL integration workspace */
+	gsl_set_error_handler_off();
+	gsl_integration_workspace *w1   = gsl_integration_workspace_alloc (10000);
+	gsl_function F;
+	F.function                      = &AT_Stopping_Power_Mass_Bethe_MeV_cm2_g_int;
+	F.params                        = (void*)&params;
+
+	/* Set integration limits */
+	double   lower_lim_m            = 0.0;
+	double   upper_lim_m            = E_MeV_u;
+	double   error;
+
+	/* Perform integration */
+	int status      = gsl_integration_qags (        &F,
+			lower_lim_m,
+			upper_lim_m,
+			1e-3,
+			1e-3,
+			10000,
+			w1,
+			&range_cm2_g,
+			&error);
+	if (status == GSL_EROUND || status == GSL_ESING){
+		printf("Error in integration of CSDA range from Bethe formula!\n");
+	}
+
+	gsl_integration_workspace_free (w1);
+
+	return(range_cm2_g);
+}
+
+double AT_WEPL_Bethe(	const double 	E_MeV_u,
+		const long 		particle_no,
+		const long 		material_no){
+	const long material_no_water = Water_Liquid;
+	return(AT_CSDA_range_Bethe_cm2_g(	E_MeV_u,
+			particle_no,
+			material_no) / AT_CSDA_range_Bethe_cm2_g(	E_MeV_u,
+					particle_no,
+					material_no_water));
+
+};
