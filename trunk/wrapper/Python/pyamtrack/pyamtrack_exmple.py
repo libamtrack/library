1ec23b664248853a74e3afcd27eb65530f4626ae
herrmaro <herrmaro@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-03-11 11:11:11 +0000

    py_wrapper:<br />  *  functions give now an output * * example added

---
+++ trunk/wrapper/Python/pyamtrack/pyamtrack_exmple.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+'''
+pyamtrack_example.py
+===========
+   Created on: 11.03.2011
+   Author: herrmann
+   
+   Example file for use of pyamtrack class.
+   
+   Requirements: Python2.6
+
+   Copyright 2006, 2011 Steffen Greilich / the libamtrack team
+
+   This file is part of the AmTrack program (libamtrack.sourceforge.net).
+
+   AmTrack is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   AmTrack is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with AmTrack (file: copying.txt).
+   If not, see <http://www.gnu.org/licenses/>
+
+'''
+
+import pyamtrack
+
+
+def main():
+    #example use of libamtrack function \'  AT_average_A_from_composition\'
+    # first have a look at the description
+    print pyamtrack.AT_average_A_from_composition.__doc__
+    result =  pyamtrack.AT_average_A_from_composition(2, [1,16], [2./18.,16./18.], 0.0)
+    
+    print result
+
+main()
