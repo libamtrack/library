a3ff468f1260947a8aea77472441d447553dce68
kongruencja <kongruencja@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-04-11 08:55:19 +0000

    Grouping of webgui widgets implemented.

--- tools/webgui/AmtrackService/src/amtrackservice/shared/Config.java
+++ tools/webgui/AmtrackService/src/amtrackservice/shared/Config.java
@@ -8,18 +8,24 @@ import amtrackservice.client.parser.AbstractXMLParser;
 public class Config extends AbstractXMLParser implements Serializable{
 
 	private static final long serialVersionUID = 928505004033338560L;
-	private HashMap<String, String> templates = new HashMap<String, String>();
+	private HashMap<String, String> templateFilenames = new HashMap<String, String>();
+	private HashMap<String, String> groupNames = new HashMap<String, String>();
 	private String serverPath;
 	private static final String PATH_WRAPPER = "wrapper/";
 	private static final String PATH_RESULTS = "result/";
 
-	public Config(String serverPath, HashMap<String, String> templates) {
+	public Config(String serverPath, HashMap<String, String> templateFilenames, HashMap<String, String> groupNames) {
 		this.serverPath = serverPath;
-		this.templates = templates;
+		this.templateFilenames = templateFilenames;
+		this.groupNames = groupNames;
 	}
 
-	public HashMap<String, String> getTemplates() {
-		return templates;
+	public HashMap<String, String> getTemplateFilenames() {
+		return templateFilenames;
+	}
+
+	public HashMap<String, String> getGroupNames() {
+		return groupNames;
 	}
 
 	public String getWrapperPath() {
