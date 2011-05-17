36615baa7851ab6c3dce2973cb0fd8cd2d067a6f
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-05-17 14:25:32 +0000

    Modified for new spc data structure

--- trunk/wrapper/R/R_package/hardcoded_wrappers/AT.SPC.spectrum.at.depth.g.cm2.R
+++ trunk/wrapper/R/R_package/hardcoded_wrappers/AT.SPC.spectrum.at.depth.g.cm2.R
@@ -1,9 +1,9 @@
 AT.SPC.spectrum.at.depth.g.cm2 <- function(spc, depth.g.cm2)
 {
-    depth.g.cm2.in.spc    <- unique(spc$data$depth.g.cm2)
+    depth.g.cm2.in.spc    <- unique(spc$depth.g.cm2)
     closest.depth.idx     <- which(abs(depth.g.cm2.in.spc - depth.g.cm2) == min(abs(depth.g.cm2.in.spc - depth.g.cm2)))
     closest.depth.g.cm2   <- depth.g.cm2.in.spc[closest.depth.idx]
-    depth.step            <- unique(spc$data$depth.step)[closest.depth.idx]
+    depth.step            <- unique(spc$depth.step)[closest.depth.idx]
 
     cat( paste( "Closest depth [g/cm2] is ", 
                 sprintf("%4.3f", closest.depth.g.cm2), 
