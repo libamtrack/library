23f70734d6d189673f9da8201e4fcaa43d907913
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-05-26 13:38:26 +0000

    Updated version number for the R package

--- wrapper/R/R_package/package/R/initial.R
+++ wrapper/R/R_package/package/R/initial.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg){
  library.dynam("libamtrack", pkg, lib)
- cat("This is libamtrack 0.5.0 'Black Wombat' (devel, r969, 2011-05-17).\nType '?libamtrack' for help.\n")
+ cat("This is libamtrack 0.5.1 'Blue Wombat' (devel, r973, 2011-05-26).\nType '?libamtrack' for help.\n")
 }
 
 .Last.lib <- function(libpath){
