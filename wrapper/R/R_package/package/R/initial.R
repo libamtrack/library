0fa8de5337890dfbb06dfeae8fa6e0eecdd6eeeb
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-05-30 14:57:50 +0000

    Modified Namespace, examples, documentation of R package according to new ability to choose stopping power data source

--- wrapper/R/R_package/package/R/initial.R
+++ wrapper/R/R_package/package/R/initial.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg){
  library.dynam("libamtrack", pkg, lib)
- cat("This is libamtrack 0.5.1 'Blue Wombat' (devel, r973, 2011-05-26).\nType '?libamtrack' for help.\n")
+ cat("This is libamtrack 0.5.1 'Blue Wombat' (devel, r988, 2011-05-30).\nType '?libamtrack' for help.\n")
 }
 
 .Last.lib <- function(libpath){
