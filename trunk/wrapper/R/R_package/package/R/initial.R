3be7072d289a96a2ae8d25f3199850a239b327ac
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-07-29 10:11:11 +0000

    Added Silicon and Copper to the material list, including PSTAR stopping power data and R package description.

--- trunk/wrapper/R/R_package/package/R/initial.R
+++ trunk/wrapper/R/R_package/package/R/initial.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg){
  library.dynam("libamtrack", pkg, lib)
- cat("This is libamtrack 0.5.1 'Blue Wombat' (devel, r997, 2011-06-15).\nType '?libamtrack' for help.\n")
+ cat("This is libamtrack 0.5.1 'Blue Wombat' (devel, r1018, 2011-06-15).\nType '?libamtrack' for help.\n")
 }
 
 .Last.lib <- function(libpath){
