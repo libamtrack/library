4e342ecbdbc61d594760ea8e06d0a45d7e968679
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-08-02 13:27:21 +0000

    Adjusted absolute error in integrator for CSDA range routine from 1e-6 m (which gave roundoff errors) to 1e-4 m (= 100 µm) which is on the upper limit of what we should tolerate.

--- trunk/wrapper/R/R_package/package/R/initial.R
+++ trunk/wrapper/R/R_package/package/R/initial.R
@@ -1,6 +1,6 @@
 .First.lib <- function(lib, pkg){
  library.dynam("libamtrack", pkg, lib)
- cat("This is libamtrack 0.5.1 'Blue Wombat' (devel, r1019, 2011-08-01).\nType '?libamtrack' for help.\n")
+ cat("This is libamtrack 0.5.1 'Blue Wombat' (devel, r1020, 2011-08-02).\nType '?libamtrack' for help.\n")
 }
 
 .Last.lib <- function(libpath){
