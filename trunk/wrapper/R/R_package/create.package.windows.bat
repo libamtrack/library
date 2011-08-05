15d199c8388adcef23c06fa2d22e6b63f4bac8b9
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-08-05 08:41:45 +0000

    Further changed to meet requirements for CRAN R package, changed additional files (README, LICENSE, etc.). Now, with having 'makevars' (POSIX) and 'makevars.win' compilation does not require any additional OS specific adjustments.

--- trunk/wrapper/R/R_package/create.package.windows.bat
+++ trunk/wrapper/R/R_package/create.package.windows.bat
@@ -3,19 +3,19 @@ REM Batch file for R package compiling under Windows
 REM ************************************************
 
 REM *** Create new temporary folder from template package structure ***
-xcopy package package.tmp /E /I /EXCLUDE:exclude.txt
+xcopy package libamtrack /E /I /EXCLUDE:exclude.txt
 
 REM *** Copy sources of libamtrack ***
-copy ..\..\..\include\*.h package.tmp\src\
-copy ..\..\..\src\*.c package.tmp\src\
+copy ..\..\..\include\*.h libamtrack\src\
+copy ..\..\..\src\*.c libamtrack\src\
 
 REM *** Copy hardcoded documentation ***
-copy .\hardcoded_documentation .\package.tmp\man\
+copy .\hardcoded_documentation .\libamtrack\man\
 
 REM *** Copy hardcoded wrappers ***
-copy .\hardcoded_wrappers\*.R .\package.tmp\R\
-copy .\hardcoded_wrappers\hardcoded_wrapper.c .\package.tmp\src\
-copy .\hardcoded_wrappers\hardcoded_wrapper.h .\package.tmp\src\
+copy .\hardcoded_wrappers\*.R .\libamtrack\R\
+copy .\hardcoded_wrappers\hardcoded_wrapper.c .\libamtrack\src\
+copy .\hardcoded_wrappers\hardcoded_wrapper.h .\libamtrack\src\
 
 REM *** Auto-generate wrappers and documentation ***
 R CMD BATCH ..\..\..\tools\automatic_wrapper_generator\collect.doxygen.information.R
@@ -24,26 +24,26 @@ R CMD BATCH ..\..\..\tools\automatic_wrapper_generator\R.generate.R.wrapper.R
 R CMD BATCH ..\..\..\tools\automatic_wrapper_generator\R.generate.Rd.documentation.R
 
 REM *** Move resulting file (wrapper C and R), dynamically coded documentation to temporary folder
-move AT_R_Wrapper.* .\package\src
-move libamtrack.R .\package\R
-move *.Rd package.tmp/man
+move AT_R_Wrapper.* .\libamtrack\src
+move libamtrack.R .\libamtrack\R
+move *.Rd libamtrack/man
 
 REM *** Copy GSL to src directory ***
-copy C:\Programme\GnuWin32\bin\libgsl.dll package.tmp\src
-copy C:\Programme\GnuWin32\bin\libgslcblas.dll package.tmp\src
+copy C:\Programme\GnuWin32\bin\libgsl.dll libamtrack\src
+copy C:\Programme\GnuWin32\bin\libgslcblas.dll libamtrack\src
 
 REM *** Build test package ***
-R CMD check package.tmp
+R CMD check libamtrack
 
 REM *** Enable for debugging ***
 REM goto :eof
 
 REM *** Build actual package ***
-R CMD INSTALL --build package.tmp
+R CMD INSTALL --build libamtrack
 
 REM Delete temporary files and folders
-del package.tmp /Q
-del package.tmp.Rcheck /Q
+del libamtrack /Q
+del libamtrack.Rcheck /Q
 del .RData
 del functions.sdd
 del ..\..\..\tools\automatic_wrapper_generator\*.Rout
