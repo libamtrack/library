7411aa3585c1625db72bc57534b7c66857bbb829
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-09-24 09:03:02 +0000

    Added / modified function for a more complete workflow with spc data, i.e. interpolating between energies. A test set of spc data (0.0.0, 12C on water, active, 3 mm RIFI) has been uploaded to the libamtrack homepage. The procedure is still faulty (depth interpolation produces double peaks, depth scaling in energy interpolation should be not linear but considering the E**2 dependency of range) and slow (AT.SPC.read being the main problem, even when only reading header information used by AT.SPC.get and AT.SPC.get.list). Added also a new script at /test/R which should be used as example and test case for spc workflow.

---
+++ wrapper/R/R_package/hardcoded_wrappers/AT.SPC.get.R
@@ -0,0 +1,21 @@
+AT.SPC.get <- function(spc.list, energy.MeV.u){
+  if(energy.MeV.u < min(spc.list$energy.MeV.u) | energy.MeV.u > max(spc.list$energy.MeV.u)){
+    cat("Requested energy outside of grid of available data.")
+    return(NULL)
+  }
+  
+  # find two closest matches
+  distance    <- abs(spc.list$energy.MeV.u - energy.MeV.u)
+  closest.idx <- match(head(sort(distance), 2), distance)
+  
+  # read and interpolate SPCs
+  spc.lower   <- AT.SPC.read( file.name        = spc.list$file.name[closest.idx[1]],,
+                              endian           = spc.list$endian[closest.idx[1]])
+  spc.upper  <- AT.SPC.read( file.name        = spc.list$file.name[closest.idx[2]],,
+                              endian           = spc.list$endian[closest.idx[2]])
+
+  spc         <- AT.SPC.interpolate(spc.lower    = spc.lower,
+                                    spc.upper   = spc.upper,
+                                    energy.MeV.u = energy.MeV.u)
+  return(spc)
+}
