7951b1f13af94872bb3012e6d2be771721ac63fe
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2011-12-15 13:42:02 +0000

    Check if R test script can handle local library instead of direct R access

--- trunk/test/R/AT_Test_CPPSC.R
+++ trunk/test/R/AT_Test_CPPSC.R
@@ -24,9 +24,8 @@
 # clear workspace
 rm( list = ls() )
 
-# Build latest version of libamtrack and load for direct access
-recompile <- TRUE
-source("AT_Test_PreRun.R")
+# Load local version of library
+require(libamtrack, lib.loc = ".")
 
 require(lattice)
 
