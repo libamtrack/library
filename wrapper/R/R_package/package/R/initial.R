ab6d0d29db54008d29d82c1193a43d84b7fd996e
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2013-01-07 10:40:13 +0000

    Fixed issue with obsolete use of .Last.Lib in R package (mail from Prof. Ripley Dec12 for R 2.14.0). Used .onUnload now instead.

--- wrapper/R/R_package/package/R/initial.R
+++ wrapper/R/R_package/package/R/initial.R
@@ -3,7 +3,7 @@
  packageStartupMessage("This is libamtrack ##VERSION## '##NAME##' (##STATUS##, ##REVISION##, ##DATE##).\nType '?libamtrack' for help.\n")
 }
 
-.Last.lib <- function(libpath){
+.onUnload <- function(libpath){
  try(library.dynam.unload("libamtrack", libpath))
 }
 
