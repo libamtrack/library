1bce746695decb38a2a74f3401f219bffe7fda29
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2013-01-21 10:31:29 +0000

    Still for 0.5.4 release: implemented R package good practice for packageStartupMessage (moved from .onLoad to .onAttach)

--- wrapper/R/R_package/package/R/initial.R
+++ wrapper/R/R_package/package/R/initial.R
@@ -1,5 +1,8 @@
 .onLoad <- function(lib, pkg){
  library.dynam("libamtrack", pkg, lib)
+}
+
+.onAttach <- function(lib, pkg){
  packageStartupMessage("This is libamtrack ##VERSION## '##NAME##' (##STATUS##, ##REVISION##, ##DATE##).\nType '?libamtrack' for help.\n")
 }
 
