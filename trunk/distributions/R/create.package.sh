61ed013d3814b5bfc8076752824898c174767a3f
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2014-08-01 12:49:37 +0000

    R package generation script now syncs with R-Forge dir

--- trunk/distributions/R/create.package.sh
+++ trunk/distributions/R/create.package.sh
@@ -4,6 +4,8 @@ echo "################################################"
 echo "This script creates the R package for libamtrack"
 echo "################################################"
 echo 
+echo "Use --help for information"
+echo
 
 ROOT_DIR=$(pwd)
 WORK_DIR=$ROOT_DIR/distributions/R
@@ -14,54 +16,44 @@ FAST_ARG="FALSE"
 INSTALL_ARG="FALSE"
 NOCLEAN_ARG="FALSE"
 NOCOMPILE_ARG="FALSE"
-SYNCWITH_ARG=""
+SYNC_ARG="FALSE"
+
 
 # set bound checking environmental variable
 export R_C_BOUNDS_CHECK=yes
 
-for var in "$@"
-do
-    if [ "$var" == "--help" ] ; then
-       HELP_ARG="TRUE"
-    fi
-
-    if [ "$var" == "--fast" ] ; then
-       FAST_ARG="TRUE"
-       echo "Fast execution chosen. Will not update svn information nor run R examples."
-    fi
-
-    if [ "$var" == "--install" ] ; then
-       INSTALL_ARG="TRUE"
-       echo "Will install package after compilation."
-    fi
-
-    if [ "$var" == "--noclean" ] ; then
-       NOCLEAN_ARG="TRUE"
-       echo "Will not remove transient files and folders."
-    fi
-
-    if [ "$var" == "--nocompile" ] ; then
-       NOCLEAN_ARG="TRUE"
-       echo "Will not remove transient files and folders."
-    fi
-
-    if [ "$var" == "--sync-with" ] ; then
-       echo "Will sync with remote package dir." 
-    fi
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+			echo "This script compiles libamtrack to an R package source tarball"
+			echo
+			echo "Use '--fast' to skip svn information update and R examples."
+			echo "Use '--noclean' to leave transient files after compilation for debugging."
+			echo "Use '--sync syncdir' to sync source package with directory syncdir."
+			echo
+			exit 0
+                        ;;
+                --fast)
+                   	FAST_ARG="TRUE"
+       			echo "Fast execution chosen. Will not update svn information nor run R examples."
+			shift
+                        ;;
+                --sync)
+                        shift
+                        if test $# -gt 0; then
+                                SYNC_ARG="TRUE"
+				SYNC_DIR=$1
+                                echo "Sync chosen with" $SYNC_DIR
+                        else
+                                echo "Error: no sync dir specified"
+                                exit 1
+                        fi
+                        shift
+			;;
+        esac
 done
-
-if [ $HELP_ARG == "TRUE" ] ; then
-   echo "This script compiles libamtrack to an R package source tarball"
-   echo
-   echo "Use --fast to skip svn information update and R examples."
-   echo "Use --install to install package after compilation."
-   echo "Use --noclean to leave transient files after compilation for debugging."
-   echo
-   exit
-fi
-
-echo "Use --help for information"
 echo
+
 
 # Clean up residues from earlier runs if applicable
 if [ -d "$WORK_DIR/libamtrack" ]; then
@@ -77,16 +69,21 @@ if [ ls $WORK_DIR/*.sdd &> /dev/null ]; then
     rm $WORK_DIR/*.sdd
 fi
 
+
 # *** Create new temporary folder from template package structure ***
 echo "Copy package template..."
 cp -r $WORK_DIR/package $WORK_DIR/libamtrack
+
+
 # Remove .svn subdirs
 find $WORK_DIR/libamtrack/ -iname .svn -print | xargs rm -rf
+
 
 # *** Copy sources of libamtrack ***
 echo "Copying libamtrack sources..."
 cp $ROOT_DIR/include/*.h $WORK_DIR/libamtrack/src/
 cp $ROOT_DIR/src/*.c $WORK_DIR/libamtrack/src/
+
 
 # *** Clean temporary files (e.g. from gedit that will confuse the collect.doxygen.information.R script)
 if [ ls $WORK_DIR/libamtrack/src/*.h~ &> /dev/null ]; then
@@ -101,19 +98,22 @@ fi
 echo "Copying hardcoded documentation..."
 cp $WORK_DIR/hardcoded_documentation/*.Rd $WORK_DIR/libamtrack/man/
 
+
 # *** Copy hardcoded wrappers ***
 echo "Copying hardcoded wrappers..."
 cp $WORK_DIR/hardcoded_wrappers/*.R $WORK_DIR/libamtrack/R/
 cp $WORK_DIR/hardcoded_wrappers/hardcoded_wrapper.c $WORK_DIR/libamtrack/src/
 cp $WORK_DIR/hardcoded_wrappers/hardcoded_wrapper.h $WORK_DIR/libamtrack/src/
 
+
 # *** Run autoconfigure (to update svn version) ***
-if [ ! -d $FAST_ARG ] ; then
+if [ $FAST_ARG == "FALSE" ] ; then
 	echo "Running autoreconf and configure in main folder (to update svnversion information)..."
 	autoreconf --force --install
 	chmod 755 configure
 	./configure
 fi
+
 
 # *** Copy generated file ***
 cp $ROOT_DIR/config.h $WORK_DIR/libamtrack/src/
@@ -160,6 +160,7 @@ mv $ROOT_DIR/AT_R_Wrapper.* $WORK_DIR/libamtrack/src
 mv $ROOT_DIR/libamtrack.R $WORK_DIR/libamtrack/R
 mv $ROOT_DIR/*.Rd $WORK_DIR/libamtrack/man
 
+
 # *** Truncate Rd files (esp. the automatically generated ones to 80 characters line width)
 echo "Running R script to truncate Rd files to 80 characters line width..."
 Rscript --no-save $SCRIPT_DIR/R.truncate.Rd.files.R $WORK_DIR/libamtrack/man >$WORK_DIR/R.truncate.Rd.files.Rout 2>&1
@@ -167,6 +168,7 @@ if [ "$?" -ne "0" ]; then
   echo "Problem with executing R.truncate.Rd.files.R"
   exit 1
 fi
+
 
 # *** Create namespace for package
 echo "Create NAMESPACE file for package..."
@@ -176,6 +178,7 @@ if [ "$?" -ne "0" ]; then
   exit 1
 fi
 
+
 # *** Run autoconf
 echo
 echo "Running autoconf to create configure script..."
@@ -184,10 +187,21 @@ autoconf
 chmod 755 configure
 cd $ROOT_DIR
 
+
+# *** Copy package directory for sync ***
+if [ $SYNC_ARG == "TRUE" ] ; then
+  echo "Syncing..."
+  cp -rp $WORK_DIR/libamtrack $ROOT_DIR/libamtrack.pkg
+  rm -rf $ROOT_DIR/libamtrack.pkg/autom4te.cache
+  rsync -avh $ROOT_DIR/libamtrack.pkg/* $SYNC_DIR
+  rm -rf $ROOT_DIR/libamtrack.pkg
+fi
+
+
 # *** Build test package ***
 echo
 echo "Running package check..."
-if [ ! -d $FAST_ARG ] ; then
+if [ $FAST_ARG == "FALSE" ] ; then
    R CMD check $WORK_DIR/libamtrack --no-manual 
    if [ "$?" -ne "0" ]; then
      echo "Problem with executing R CMD check ./package --no-manual"
@@ -212,8 +226,9 @@ echo
 echo "Building source package tarball..."
 R CMD build $WORK_DIR/libamtrack
 
+
 # *** Remove transient files ***
-if [ ! $NOCLEAN_ARG == "TRUE" ] ; then
+if [ $NOCLEAN_ARG == "FALSE" ] ; then
 	echo
 	echo "Removing transient files and folder..."
 	rm -f -r $WORK_DIR/libamtrack
@@ -223,8 +238,4 @@ if [ ! $NOCLEAN_ARG == "TRUE" ] ; then
 	rm -f $WORK_DIR/*.RData
 fi
 
-# *** Install package if chose ***
-if [ $INSTALL_ARG == "TRUE" ] ; then
-    sudo R CMD INSTALL $ROOT_DIR/libamtrack_*.tar.gz
-fi
 echo "Done!"
