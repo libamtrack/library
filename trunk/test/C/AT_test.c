bcb6900137804098f38aead16003d2d8ceffeb58
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2015-04-20 12:07:53 +0000

    Added ICUR49/73 stopping powers for alumina (CAVE: mix of E per n and E per amu [Z>=3]). Prepared routine to read in stopping power tables during run-time.

--- trunk/test/C/AT_test.c
+++ trunk/test/C/AT_test.c
@@ -30,6 +30,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
 #include <stdbool.h>
 #include <string.h>
@@ -46,9 +47,58 @@
 
 // CAVE: Remove this by appropriate include file when using other functions from libamtrack
 // As it can trigger very strange behaviour such as faulty return values etc.
-#include "AT.h"
+// #include "AT.h"
 
 int main(){
+	 FILE *CSV;
+  	 CSV = fopen("FLUKA_DEDX_WATER_76.8eV.txt", "r");
 
+	 if(NULL == CSV) {
+		 fprintf(stderr, "Error opening stopping power data file ...\n");
+		 return EXIT_FAILURE;
+	 }
+
+
+	 char 	line[1000], test[1000];
+
+	 int  	i = 0, ZZ;
+	 float	EE, SS;
+
+	 int	Z[10000];
+	 float	E[10000];
+	 float  S[10000];
+
+	 while (fgets(line, sizeof line, CSV)) {
+	     if (*line == EOF) break;
+	     if (isalpha(*line)) continue; /* ignore comment line */
+	     	 sscanf(line, "%s", test);
+	     	 if ((sscanf(line, "%d %e %e", &ZZ, &EE, &SS) != 3)&&(strlen(test) != 0)&&(strlen(test) != 1)) {
+	         /* handle error */
+				 printf("Error reading stopping power data (format correct?)...");
+				 return EXIT_FAILURE;
+			 } else {
+			 /* handle variables */
+				 Z[i] = ZZ;
+				 E[i] = EE;
+				 S[i++] = SS;
+			 }
+	 	 }
+
+	 printf("Read %d lines.\n", i--);
+
+
+	 int* Z_array = (int*)malloc(i*sizeof(int));
+	 float* E_array = (float*)malloc(i*sizeof(float));
+	 float* S_array = (float*)malloc(i*sizeof(float));
+
+	 memcpy(Z_array, Z, i*sizeof(int));
+	 memcpy(E_array, E, i*sizeof(float));
+	 memcpy(S_array, S, i*sizeof(float));
+
+	 for(int j = 0; j < i; j++){
+		 printf("line %d: %d %e %e\n", j, Z_array[j], E_array[j], S_array[j]);
+	 }
+
+	 return EXIT_SUCCESS;
 };
 
