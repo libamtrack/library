1e90284391781ca510c157458ca2b368de97ffcd
sgreilich <sgreilich@157e7b92-8bc4-4139-a03b-5f845c8402eb>
2015-06-20 05:37:37 +0000

    Added inverse distribution functions for energy loss straggling

--- trunk/thirdparty/cernlib/AT_CernlibFuns.h
+++ trunk/thirdparty/cernlib/AT_CernlibFuns.h
@@ -34,8 +34,10 @@
 #include <stdio.h>
 
 double CL_denlan( double lambda_landau );
+double CL_ranlan( double rnd );
 
 void   CL_vavset( double kappa, double beta2 );
 double CL_vavden( double lambda_vavilov );
+double CL_vavran( double kappa, double beta2, double rnd );
 
 #endif /* AT_CERNLIBFUNS_H_ */
