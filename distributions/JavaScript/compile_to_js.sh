ddaae85d81ac9f9cb6a4c35b5095c93565bd502f
marwin1991 <peter.zmilczak@gmail.com>
2018-12-22 07:10:16 -0500

    Moved JS files to distributions/JavaScript/ Change emcc to use only libgsl.a no need of libgsl.so Added WASM param to compile_to_js.sh to let compile without Used ALL_FUNCTIONS flag in emcc instead of generating list Rewrite instruction to compile to JS JS compiled library now appears in /distributions/JavaScript/output/ Removed unsed libgsl.so

---
+++ distributions/JavaScript/compile_to_js.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#!/bin/bash
+if [ "$1" != "" ]; then
+    echo "WASM parameter set to $1"
+    WASM=$1
+else
+    echo "WASM parameter EMPTY !!! Default set to 1"
+    WASM=1
+fi
+
+emcmake="/home/osboxes/emsdk/emscripten/1.37.36/./emcmake"
+emmake="/home/osboxes/emsdk/emscripten/1.37.36/./emmake"
+emcc="/home/osboxes/emsdk/emscripten/1.37.36/./emcc"
+
+cd ../../
+mkdir _build
+cd _build
+cp ../distributions/JavaScript/libgsl.a .
+ls .
+$emcmake cmake ..
+$emmake make
+
+
+$emcc libat.a libgsl.a -o libat.html -s WASM=$WASM -s EXPORT_ALL=1 \
+-s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'
+
+rm ../distributions/JavaScript/output/*
+cp libat.a ../distributions/JavaScript/output/
+cp libat.html ../distributions/JavaScript/output/
+cp libat.wasm ../distributions/JavaScript/output/ 2>/dev/null || : #ignore error when build with -s WASM=0
+cp libat.js ../distributions/JavaScript/output/
+
+cd ..
+rm -r _build
