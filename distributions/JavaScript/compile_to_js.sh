60a65cb72a3566f04df53f5d9bc779b0e8edc52b
Piotr Zmilczak <peter.zmilczak@gmail.com>
2018-12-22 15:15:40 +0100

    Update compile_to_js.sh

--- distributions/JavaScript/compile_to_js.sh
+++ distributions/JavaScript/compile_to_js.sh
@@ -9,20 +9,20 @@ else
     WASM=1
 fi
 
-emcmake="/home/osboxes/emsdk/emscripten/1.37.36/./emcmake"
-emmake="/home/osboxes/emsdk/emscripten/1.37.36/./emmake"
-emcc="/home/osboxes/emsdk/emscripten/1.37.36/./emcc"
+#emcmake="/home/osboxes/emsdk/emscripten/1.37.36/./emcmake"
+#emmake="/home/osboxes/emsdk/emscripten/1.37.36/./emmake"
+#emcc="/home/osboxes/emsdk/emscripten/1.37.36/./emcc"
 
 cd ../../
 mkdir _build
 cd _build
 cp ../distributions/JavaScript/libgsl.a .
 ls .
-$emcmake cmake ..
-$emmake make
+emcmake cmake ..
+emmake make
 
 
-$emcc libat.a libgsl.a -o libat.html -s WASM=$WASM -s EXPORT_ALL=1 \
+emcc libat.a libgsl.a -o libat.html -s WASM=$WASM -s EXPORT_ALL=1 \
 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'
 
 rm ../distributions/JavaScript/output/*
