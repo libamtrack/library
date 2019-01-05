5d3c1cbdbe968c8c6ee13f7c5edcbe492d921175
Piotr Zmilczak <peter.zmilczak@gmail.com>
2019-01-05 12:23:42 +0100

    Added some doxygen comments to make travis pass

--- include/AT_CernlibFuns.h
+++ include/AT_CernlibFuns.h
@@ -1,20 +1,44 @@
-//
-// Created by osboxes on 04.09.18.
-//
-
+//Mock of funcions from thirdparty/cernlib
 #ifndef LIBRARY_AT_CERNLIBFUNS_H
 #define LIBRARY_AT_CERNLIBFUNS_H
 
 #include <stdio.h>
 
+/**
+ * TODO
+ * @param[in]  lambda_landau
+ * @return     Landau 
+ */
 double CL_denlan(double lambda_landau);
 
+/**
+ * TODO
+ * @param[in]  rnd
+ * @return     Landau random number
+ */
 double CL_ranlan(double rnd);
 
+/**
+ * TODO
+ * @param[in]  kappa
+ * @param[out]  beta2
+ */
 void CL_vavset(double kappa, double beta2);
 
+/**
+ * TODO
+ * @param[in]  lambda_vavilov
+ * @return     Vavilov 
+ */
 double CL_vavden(double lambda_vavilov);
 
+/**
+ * TODO
+ * @param[in]  kappa
+ * @param[in]  beta2
+ * @param[in]  rnd
+ * @return     Vavilov 
+ */
 double CL_vavran(double kappa, double beta2, double rnd);
 
 #endif //LIBRARY_AT_CERNLIBFUNS_H
