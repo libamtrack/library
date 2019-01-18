539378610f634aded3ce745cec22e971331449f2
Leszek Grzanka <leszek.grzanka@ifj.edu.pl>
2019-01-18 23:09:26 +0100

    travis improved

---
+++ distributions/JavaScript/trigger_docker_rebuild.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d '{ "request": { "branch":"master", "message": "Build Travis after changes in libamtrack/library" }}' https://api.travis-ci.com/repo/libamtrack%2FDockerFiles/requests
