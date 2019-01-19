46e4d92e655b96a70039f0c521737df57d741fd0
Piotr Zmilczak <peter.zmilczak@gmail.com>
2019-01-19 17:15:04 +0100

    change forjs to master in trigger_docker_rebuild.sh

--- distributions/JavaScript/trigger_docker_rebuild.sh
+++ distributions/JavaScript/trigger_docker_rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "forjs" ]]  #in future change "forjs" to "master"
+if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]
 then
   curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_TOKEN" -d '{ "request": { "branch":"master", "message": "Build Travis after changes in libamtrack/library" }}' https://api.travis-ci.org/repo/libamtrack%2FDockerFiles/requests
 fi
