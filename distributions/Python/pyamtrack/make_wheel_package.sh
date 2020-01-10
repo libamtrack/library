1f41ac3448f6c27e70e7753f8aacecb293a34131
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-01-10 23:10:42 +0100

    new files

--- distributions/Python/pyamtrack/make_wheel_package.sh
+++ distributions/Python/pyamtrack/make_wheel_package.sh
@@ -2,5 +2,8 @@
 
 mkdir workspace
 cp run_me_inside_docker.sh workspace
-docker run --rm -e PLAT=manylinux1_x86_64 -v workspace:/io quay.io/pypa/manylinux1_x86_64 run_me_inside_docker.sh
+git clone https://github.com/Tetrite/cBinder.git workspace
 
+docker run --rm -e PLAT=manylinux1_x86_64 -v workspace:/io quay.io/pypa/manylinux1_x86_64 /io/run_me_inside_docker.sh
+
+ls -alh workspace
