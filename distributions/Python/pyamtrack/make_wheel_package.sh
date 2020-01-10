70644db78f8c909c7f6fd0df78e35710d10da0a4
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-01-10 23:19:42 +0100

    better scripts

--- distributions/Python/pyamtrack/make_wheel_package.sh
+++ distributions/Python/pyamtrack/make_wheel_package.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir workspace
-cp run_me_inside_docker.sh workspace
-git clone https://github.com/Tetrite/cBinder.git workspace
+#mkdir workspace
+#cp run_me_inside_docker.sh workspace
+git clone https://github.com/Tetrite/cBinder.git
 
-docker run --rm -e PLAT=manylinux1_x86_64 -v workspace:/io quay.io/pypa/manylinux1_x86_64 /io/run_me_inside_docker.sh
+docker run --rm -e PLAT=manylinux1_x86_64 -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/run_me_inside_docker.sh
 
 ls -alh workspace
--- distributions/Python/pyamtrack/run_me_inside_docker.sh
+++ distributions/Python/pyamtrack/run_me_inside_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pwd
-
-CFLAGS=-std=c99 /opt/python/cp36-cp36m/bin/python3 /io/cBinder/cBinder/main.py -v pyamtrack -f ../library/src/ -f ../library/include/ -d generated -es ../cBinder/tests/libamtrack/libamtrack_export_symbols_full.txt  -mono libAT compile -i /io/gsl/include/ -i ../library/include/ -b /io/gsl/lib/ -l gsl -l gslcblas -l m -e 'std=c99'
+/opt/python/cp36-cp36m/bin/python3 -m pip install /io/cBinder/
+CFLAGS=-std=c99 /opt/python/cp36-cp36m/bin/python3 -m cBinder -v pyamtrack -f ../library/src/ -f ../library/include/ -d generated -es ../cBinder/tests/libamtrack/libamtrack_export_symbols_full.txt  -mono libAT compile -i /io/gsl/include/ -i ../library/include/ -b /io/gsl/lib/ -l gsl -l gslcblas -l m -e 'std=c99'
