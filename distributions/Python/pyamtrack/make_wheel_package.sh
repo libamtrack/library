d7794aa69c88de9ba3f30de0ca069eaed088592f
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-01-10 23:53:32 +0100

    more fixes, package install

--- distributions/Python/pyamtrack/make_wheel_package.sh
+++ distributions/Python/pyamtrack/make_wheel_package.sh
@@ -8,7 +8,7 @@ mkdir libamtrack
 cp -r ../../../src libamtrack/
 cp -r ../../../include libamtrack/
 
-wget http://ftp.task.gda.pl/pub/gnu/gsl/gsl-latest.tar.gz
+wget -q http://ftp.task.gda.pl/pub/gnu/gsl/gsl-latest.tar.gz
 tar -zxvf gsl-latest.tar.gz
 
 docker run --rm -e PLAT=manylinux1_x86_64 -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/run_me_inside_docker.sh
