3b95af29ad18752a2321e9a60236306f46245c51
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-01-13 21:48:25 +0100

    version with comments

--- distributions/Python/pyamtrack/make_wheel_package.sh
+++ distributions/Python/pyamtrack/make_wheel_package.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-#mkdir workspace
-#cp run_me_inside_docker.sh workspace
+# get the tool which aids making wheel package
+# it will automatically generate python wrapper for decorated (doxygen) C code
+rm -rf cBinder
 git clone https://github.com/Tetrite/cBinder.git
 
+# copy source files and headers to local folder, so the docker container can access them
+rm -rf libamtrack
 mkdir libamtrack
-cp -r ../../../src libamtrack/
-cp -r ../../../include libamtrack/
+cp -rf ../../../src libamtrack/
+cp -rf ../../../include libamtrack/
 
-wget -q http://ftp.task.gda.pl/pub/gnu/gsl/gsl-latest.tar.gz
-tar -zxvf gsl-latest.tar.gz
+# get necessary dependency (latest) version of GSL library
+wget -q "http://ftpmirror.gnu.org/gnu/gsl/gsl-latest.tar.gz" -O gsl.tar.gz
+tar -zxvf gsl.tar.gz
+# make directory for GSL installation
+rm -rf gsl
+mkdir gsl
 
+# run the docker process which using manylinux1 machine (SLC6 based) will compile
+# the C extensions using old enough libraries
+# in such way the will be as portable as possible
+rm -rf generated
 docker run --rm -e PLAT=manylinux1_x86_64 -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/run_me_inside_docker.sh
 
+# list the generated files
 ls -alh generated/dist/
