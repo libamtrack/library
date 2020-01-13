7a8c3350bb300f3ed9a765d4c40483039334afa8
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-01-14 00:13:27 +0100

    package deploy

---
+++ distributions/Python/pyamtrack/deploy_package.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x # Print command traces before executing command
+
+set -e # Exit immediately if a simple command exits with a non-zero status.
+
+set -o pipefail # Return value of a pipeline as the value of the last command to
+                # exit with a non-zero status, or zero if all commands in the
+                # pipeline exit successfully.
+
+
+write_pypirc() {
+PYPIRC=~/.pypirc
+
+if [ -e "${PYPIRC}" ]; then
+    rm ${PYPIRC}
+fi
+
+touch ${PYPIRC}
+cat <<pypirc >${PYPIRC}
+[distutils]
+index-servers =
+    pypi
+[pypi]
+username: ${PYPIUSER}
+password: ${PYPIPASS}
+pypirc
+
+if [ ! -e "${PYPIRC}" ]; then
+    echo "ERROR: Unable to write file ~/.pypirc"
+    exit 1
+fi
+}
+
+# write .pypirc file with pypi repository credentials
+set +x
+write_pypirc
+set -x
+
+echo "User" $PYPIUSER
+
+# make a source package
+pip install -U twine
+
+# upload only if tag present
+if [[ $TRAVIS_TAG != "" ]]; then
+  twine upload $TRAVIS_BUILD_DIR/distributions/Python/pyamtrack/generated/dist/wheelhouse/*.whl
+fi
--- distributions/Python/pyamtrack/test_inside_docker.sh
+++ distributions/Python/pyamtrack/test_inside_docker.sh
@@ -5,4 +5,4 @@ apt install -y --no-install-recommends python3-pip
 python3 -V
 pip3 -V
 find /io/generated/dist/wheelhouse/ -name "*.whl" -exec pip3 install {} \;
-python3 -c "from libamtrack import libAT; a = libAT.AT_lambda_Landau_Mode(); print(a);"
+python3 -c "from pyamtrack import libAT; a = libAT.AT_lambda_Landau_Mode(); print(a);"
