789edcb38a43dd080397bcba20c114adaadc70e5
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-01-14 01:01:52 +0100

    cBinder PoC (#76)
    * initial commit, cleaning, travis updated
    
    * travis fixes
    
    * sudo added
    
    * new files
    
    * better scripts
    
    * GSL added
    
    * small fix
    
    * small fixes
    
    * python added
    
    * python check
    
    * more fixes, package install
    
    * python test
    
    * pip test removed
    
    * cleaning
    
    * version with comments
    
    * custom setup.py
    
    * small fixes
    
    * tests added
    
    * package deploy
    
    * fix requirements
    
    * correct username
    
    * user grzanka back
    
    * setuptools added
    
    * wheel added
    
    * test

---
+++ distributions/Python/pyamtrack/generate_inside_docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# compile GSL sources
+cd /io/gsl-2.6
+./configure --prefix=/io/gsl
+make -j2
+make install
+
+# install cBinder package from a local folder
+/opt/python/cp36-cp36m/bin/python3 -m pip install /io/cBinder/
+
+# generate extension and make wheel package
+rm -rf /io/generated
+CFLAGS='-std=c99' /opt/python/cp36-cp36m/bin/python3 -m cBinder pyamtrack \
+-f /io/libamtrack/src/ \
+-f /io/libamtrack/include/ \
+-d /io/generated \
+-es /io/symbols_to_export.txt  \
+-mono libAT \
+compile \
+-i /io/gsl/include/ \
+-i /io/libamtrack/include/ \
+-b /io/gsl/lib/ \
+-l gsl -l gslcblas -l m
+
+# generate package once more with adjusted setup.py
+cp /io/setup.py /io/generated
+cd /io/generated
+rm -rf build
+rm -rf dist
+/opt/python/cp36-cp36m/bin/python3 setup.py bdist_wheel
+
+# add manylinux1 tag
+cd /io/generated/dist
+auditwheel repair *whl
---
+++ distributions/Python/pyamtrack/make_wheel_package.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# get the tool which aids making wheel package
+# it will automatically generate python wrapper for decorated (doxygen) C code
+rm -rf cBinder
+git clone https://github.com/Tetrite/cBinder.git
+
+# copy source files and headers to local folder, so the docker container can access them
+rm -rf libamtrack
+mkdir libamtrack
+cp -rf ../../../src libamtrack/
+cp -rf ../../../include libamtrack/
+
+# get necessary dependency (latest) version of GSL library
+wget -q "http://ftpmirror.gnu.org/gnu/gsl/gsl-latest.tar.gz" -O gsl.tar.gz
+tar -zxvf gsl.tar.gz
+# make directory for GSL installation
+rm -rf gsl
+mkdir gsl
+
+# run the docker process which using manylinux1 machine (SLC6 based) will compile
+# the C extensions using old enough libraries
+# in such way the will be as portable as possible
+rm -rf generated
+docker run --rm -e PLAT=manylinux1_x86_64 -v `pwd`:/io quay.io/pypa/manylinux1_x86_64 /io/generate_inside_docker.sh
+
+# list the generated files
+ls -alh generated/dist/
+ls -alh generated/dist/wheelhouse
