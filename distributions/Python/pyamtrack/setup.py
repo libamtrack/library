7d4551f3c0dc52de6943d5a596ea284d6521c9a9
Leszek Grzanka <leszek.grzanka@gmail.com>
2020-01-18 15:10:06 +0100

    version bump

--- distributions/Python/pyamtrack/setup.py
+++ distributions/Python/pyamtrack/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pyamtrack",
-    version="0.1.2",
+    version="0.1.3",
     packages=['pyamtrack'],
     package_data={'': ['lib/*']},
     python_requires='>=3.6',
