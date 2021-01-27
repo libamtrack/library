ecf41ab1b5753f597dbb6647302f43fa8770d1c7
Leszek Grzanka <leszek.grzanka@gmail.com>
2021-01-27 17:06:34 +0100

    more cleaning

--- test/C/AT_test.c
+++ test/C/AT_test.c
@@ -30,79 +30,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <math.h>
-#include <stdbool.h>
-#include <string.h>
-#include <assert.h>
-
-// Some headers are found in different places in Mac OS X
-#ifdef __APPLE__
-	#include <sys/malloc.h>
-#else
-	#include <malloc.h>
-#endif
-
 
 #include "AT_StoppingPower.h"
-//#include "AT_CernlibFuns.h"
 #include "AT_DataRange.h"
 
-int main(){
+int main() {
 
-	double Test = AT_CSDA_range_g_cm2_single( 270.55,
-		0.1,
-		6012,
-		1);
-        
-        printf("Range of 270.55 C-12 in water: %3.2f cm\n\n", Test);
-        
-        const double E_MeV_u[3]   = {1, 10, 100};
-	const long particle_no[3] = {6012, 6012, 6012};
-	const long material_no = 2;
+    double Test = AT_CSDA_range_g_cm2_single(270.55,
+                                             0.1,
+                                             6012,
+                                             1);
 
-	double test[3];
+    printf("Range of 270.55 C-12 in water: %3.2f cm\n\n", Test);
 
-	AT_Mass_Stopping_Power("/home/greilich/R/x86_64-pc-linux-gnu-library/3.2/libamtrack/extdata/FLUKA_DEDX_WATER_76.8eV.txt",
-			3,
-			E_MeV_u,
-			particle_no,
-			2,
-			test);
+    const double E_MeV_u[3] = {1, 10, 100};
+    const long particle_no[3] = {6012, 6012, 6012};
+    const long material_no = 2;
 
-	printf("Ergebnis FromFile: %e, %e, %e\n", test[0], test[1], test[2]);
+    double test[3];
 
-	AT_Mass_Stopping_Power("Bethe",
-			3,
-			E_MeV_u,
-			particle_no,
-			material_no,
-			test);
-	printf("Ergebnis Bethe: %e, %e, %e\n", test[0], test[1], test[2]);
-
-	AT_Mass_Stopping_Power("PSTAR",
-			3,
-			E_MeV_u,
-			particle_no,
-			material_no,
-			test);
-	printf("Ergebnis PSTAR: %e, %e, %e\n", test[0], test[1], test[2]);
-
-	AT_Mass_Stopping_Power("ICRU",
-			3,
-			E_MeV_u,
-			particle_no,
-			material_no,
-			test);
-	printf("Ergebnis ICRU: %e, %e, %e\n", test[0], test[1], test[2]);
-
-//	double l = 0.0, kappa = 1.0, beta = 0.5;
-//	CL_vavset(kappa, beta*beta);
+//    AT_Mass_Stopping_Power(
+//            "/home/greilich/R/x86_64-pc-linux-gnu-library/3.2/libamtrack/extdata/FLUKA_DEDX_WATER_76.8eV.txt",
+//            3,
+//            E_MeV_u,
+//            particle_no,
+//            2,
+//            test);
 //
-//	printf("Ergebnis Landau / Vavilov: %e, %e\n", CL_denlan(l), CL_vavden(l));
+//    printf("Ergebnis FromFile: %e, %e, %e\n", test[0], test[1], test[2]);
 
+    AT_Mass_Stopping_Power("Bethe",
+                           3,
+                           E_MeV_u,
+                           particle_no,
+                           material_no,
+                           test);
+    printf("Bethe: %e, %e, %e\n", test[0], test[1], test[2]);
 
+    AT_Mass_Stopping_Power("PSTAR",
+                           3,
+                           E_MeV_u,
+                           particle_no,
+                           material_no,
+                           test);
+    printf("PSTAR: %e, %e, %e\n", test[0], test[1], test[2]);
 
-	return EXIT_SUCCESS;
+    AT_Mass_Stopping_Power("ICRU",
+                           3,
+                           E_MeV_u,
+                           particle_no,
+                           material_no,
+                           test);
+    printf("ICRU: %e, %e, %e\n", test[0], test[1], test[2]);
+
+    return EXIT_SUCCESS;
 };
 
