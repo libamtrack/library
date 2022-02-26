fa1fa5f337bb08fd79c46aa13eb34d2ae09bd9fc
Leszek Grzanka <grzanka@agh.edu.pl>
2022-02-26 10:11:00 +0000

    new tests

--- distributions/R/run.sh
+++ distributions/R/run.sh
@@ -5,4 +5,6 @@ source venv/bin/activate
 
 python -m pip install -r generator/requirements.txt
 
+python prepare.py --desc-path ./DESCRIPTION --version 0.12.0 --out-dir ./out ../../include ../../src
+
 deactivate
