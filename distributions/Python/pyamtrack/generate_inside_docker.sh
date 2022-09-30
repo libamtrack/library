0048c5a2accaafc9a2035f9e1186a5f06e9e3bed
Leszek Grzanka <grzanka@agh.edu.pl>
2022-09-30 08:57:12 +0000

    fixes

--- distributions/Python/pyamtrack/generate_inside_docker.sh
+++ distributions/Python/pyamtrack/generate_inside_docker.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # compile GSL sources
-cd /io/gsl-latest
-./configure --prefix=/io/gsl
+cd /io/gsl-latest || exit 1
+./configure --prefix=/io/gsl || exit 1
 #  -j [N], --jobs[=N]          Allow N jobs at once; infinite jobs with no arg.
-make -j
-make install
+make -j || exit 1
+make install || exit 1
 
 # install cBinder package from a local folder
-/opt/python/cp36-cp36m/bin/python3 -m pip install /io/cBinder/
+/opt/python/cp36-cp36m/bin/python3 -m pip install /io/cBinder/ || exit 1
 
 # generate extension and make wheel package
-rm -rf /io/generated
-mkdir /io/generated
-cd /io/generated
+rm -rf /io/generated || exit 1
+mkdir /io/generated || exit 1
+cd /io/generated || exit 1
 CFLAGS='-std=c99' /opt/python/cp36-cp36m/bin/python3 -m cBinder pyamtrack \
 -f /io/libamtrack/src/ \
 -f /io/libamtrack/include/ \
@@ -24,29 +24,29 @@ compile \
 -i /io/gsl/include/ \
 -i /io/libamtrack/include/ \
 -b /io/gsl/lib/ \
--l gsl -l gslcblas -l m
+-l gsl -l gslcblas -l m || exit 1
 
 # fix git tags and create clean repo
-cd /io/libamtrack
+cd /io/libamtrack || exit 1
 TAG=`git describe --tags --abbrev=0`
-rm -rf .git
-git init
-git add *
-git config --global user.email "leszek.grzanka@ifj.edu.pl"
-git config --global user.name "Leszek Grzanka"
-git commit -m "Clean commit"
-git tag -a $TAG -m "Tag"
+rm -rf .git || exit 1
+git init || exit 1
+git add * || exit 1
+git config --global user.email "leszek.grzanka@ifj.edu.pl" || exit 1
+git config --global user.name "Leszek Grzanka" || exit 1
+git commit -m "Clean commit" || exit 1
+git tag -a $TAG -m "Tag" || exit 1
 
 # generate package once more with adjusted setup.py
 # pure `cp` ask for confirmation to overwrite file even with `-f` option
 # this may be due to the fact that `cp` was aliased to `cp -i` (interactive)
 # therefore we avoid this problem by using pure `/bin/cp`
-/bin/cp --force /io/setup.py /io/generated
-cd /io/generated
-rm -rf build
-rm -rf dist
-/opt/python/cp36-cp36m/bin/python3 setup.py bdist_wheel
+/bin/cp --force /io/setup.py /io/generated || exit 1
+cd /io/generated || exit 1
+rm -rf build || exit 1
+rm -rf dist || exit 1
+/opt/python/cp36-cp36m/bin/python3 setup.py bdist_wheel || exit 1
 
 # add manylinux1 tag
-cd /io/generated/dist
-auditwheel repair *whl
+cd /io/generated/dist || exit 1
+auditwheel repair *whl || exit 1
--- distributions/Python/pyamtrack/setup.py
+++ distributions/Python/pyamtrack/setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     use_scm_version= {
         "root": "../libamtrack/",
         "relative_to": __file__,
-        "version_scheme": "{next_version}",
+#        "version_scheme": "{next_version}",
         "local_scheme": "no-local-version"
     },
     setup_requires=['cffi>=1.13.0', 'setuptools_scm<7'],
