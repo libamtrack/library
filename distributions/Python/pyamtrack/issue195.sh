15691a5566284cfd1a61ad229d0e8169df030290
Leszek Grzanka <grzanka@agh.edu.pl>
2022-10-03 19:13:51 +0000

    cleaning

--- distributions/Python/pyamtrack/issue195.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo ./make_wheel_package.sh || exit 1
-python -m pip install numpy || exit 1
-python -m pip install --force-reinstall  generated/dist/wheelhouse/pyamtrack-0.13.0-py3-none-manylinux_2_5_x86_64.manylinux1_x86_64.whl || exit 1
-python issue195.py || exit 1
