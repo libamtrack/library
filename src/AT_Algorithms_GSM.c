d7f3c9aff06fa393dd1464924dcbf7d6a33def0e
Leszek Grzanka <grzanka@agh.edu.pl>
2022-10-03 19:28:18 +0000

    fix

--- src/AT_Algorithms_GSM.c
+++ src/AT_Algorithms_GSM.c
@@ -29,7 +29,6 @@
  */
 
 #include "AT_Algorithms_GSM.h"
-#include <time.h>
 
 void AT_GSM_sample_particle_positions(  const long  number_of_field_components,
                 const double         fluence_cm2[],
@@ -38,8 +37,6 @@ void AT_GSM_sample_particle_positions(  const long  number_of_field_components,
                 long                 number_of_particles_in_field_component[],
                 double*              x_position[],
                 double*              y_position[]){
-
-        printf("start AT_GSM_sample_particle_positions\n");
 
         /* Create and initialize random number generator */
         gsl_rng * rng  =  gsl_rng_alloc(gsl_rng_taus);
@@ -57,7 +54,6 @@ void AT_GSM_sample_particle_positions(  const long  number_of_field_components,
 
         long i;
         double sample_grid_area_cm2 = gsl_pow_2(100.0 * sample_grid_size_m);
-        printf("number of field components: %ld\n", number_of_field_components);
         for (i = 0; i < number_of_field_components; i++){
                 mean_number_particles_in_field_component   =  sample_grid_area_cm2 * total_fluence_cm2 * norm_fluence[i];
 
@@ -65,7 +61,6 @@ void AT_GSM_sample_particle_positions(  const long  number_of_field_components,
                 number_of_particles_in_field_component[i]  =  (long)gsl_ran_poisson(rng, mean_number_particles_in_field_component);
 
                 /* Now we know how many particles from one component we have, so position tables can be allocated */
-                printf("number of particles in field component %ld: %ld\n", i, number_of_particles_in_field_component[i]);
                 x_position[i] =  (double*)calloc(number_of_particles_in_field_component[i], sizeof(double));
                 y_position[i] =  (double*)calloc(number_of_particles_in_field_component[i], sizeof(double));
         }
@@ -78,14 +73,12 @@ void AT_GSM_sample_particle_positions(  const long  number_of_field_components,
                         x_position[i][j] = gsl_rng_uniform_pos(rng) * sample_grid_size_m;
                         y_position[i][j] = gsl_rng_uniform_pos(rng) * sample_grid_size_m;
                 }
-                printf("x_position[%ld][0] = %f\n", i, x_position[i][0]);
         }
 
         // get random integer as next seed
         *random_number_generator_seed = gsl_rng_get(rng);
 
         gsl_rng_free(rng);
-        printf("stop AT_GSM_sample_particle_positions\n");
 }
 
 
@@ -105,26 +98,16 @@ void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_comp
     double**       grid_D_Gy){
 
   long i,j,k,l;
-  time_t begin, end;
-  printf("start AT_GSM_dose_grid_from_particles_positions\n");
 
-  begin = time(NULL);
   /* calculate maximum delta-electron range for all components */
   double* r_max_m   = (double*)calloc(number_of_field_components, sizeof(double));
   AT_max_electron_ranges_m( number_of_field_components, E_MeV_u, material_no, er_model, r_max_m );
-
-  end = time(NULL);
-  printf("step1 time %f [s]\n", difftime(end, begin));
 
   /* find maximum of maximal delta-electron ranges */
   double max_r_max_m = 0.0;
   for (i = 0; i < number_of_field_components; i++){
     max_r_max_m    =   GSL_MAX(max_r_max_m, r_max_m[i]);
   }
-
-  end = time(NULL);
-  printf("step2 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
 
   /* allocate memory for vector of vectors distances between track cores and grid points */
   double**  distances   = (double**)calloc(number_of_field_components, sizeof(double*));
@@ -134,15 +117,11 @@ void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_comp
 
   /* vector of indices for distances vectors */
   long* distances_index = (long*)calloc(number_of_field_components, sizeof(long));
-  
+
   for (i = 0; i < number_of_field_components; i++){
     number_of_visible_pixels[i] = 0;
     distances_index[i] = 0;
   }
-
-  end = time(NULL);
-  printf("step3 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
 
   /* calculate how many grid pixels are visible for all particles from given component */
   for (j = 0; j < nX; j++){          // y
@@ -151,8 +130,8 @@ void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_comp
       double cur_x_pos =  max_r_max_m + ((double)i + 0.5)*pixel_size_m;
       for (k = 0; k < number_of_field_components; k++){  // field components
         for (l = 0; l < number_of_particles_in_field_component[k]; l++){  // particles
-          double r_squared_m  =  gsl_pow_2(x_position[k][l] - cur_x_pos) + gsl_pow_2(y_position[k][l] - cur_y_pos);
-          if(r_squared_m <= gsl_pow_2(r_max_m[k])){    // does particle contribute?, we compare r^2 with r_max^2 to avoid CPU costly sqrt
+          double r_m  =  sqrt( gsl_pow_2(x_position[k][l] - cur_x_pos) + gsl_pow_2(y_position[k][l] - cur_y_pos));
+          if(r_m <= r_max_m[k]){    // does particle contribute?
             number_of_visible_pixels[k]++;
           }
         } // particle contribution
@@ -160,17 +139,9 @@ void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_comp
     } // x loop
   } // y loop
 
-  end = time(NULL);
-  printf("step4 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
-
   for (k = 0; k < number_of_field_components; k++){
     distances[k] = (double*)calloc( number_of_visible_pixels[k], sizeof(double) );
   }
-
-  end = time(NULL);
-  printf("step5 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
 
   /* save distances between visible grid pixels and all particles from given component in linear vectors */
   for (j = 0; j < nX; j++){          // y
@@ -190,11 +161,6 @@ void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_comp
   } // y loop
 
   /* for every component calculate doses delivered to points in given distance from particle track */
-  
-  end = time(NULL);
-  printf("step6 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
-
   double**  doses = (double**)calloc(number_of_field_components, sizeof(double*));
   for (i = 0; i < number_of_field_components; i++){
     doses[i] = (double*)calloc( number_of_visible_pixels[i], sizeof(double) );
@@ -211,10 +177,6 @@ void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_comp
         doses[i]);
   }
 
-  end = time(NULL);
-  printf("step7 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
-
   for (i = 0; i < number_of_field_components; i++){
     free(distances[i]);
     distances_index[i] = 0;
@@ -222,10 +184,6 @@ void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_comp
 
   free(distances);
   free(number_of_visible_pixels);
-
-  end = time(NULL);
-  printf("step8 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
 
   /* sum up doses for every grid cell from every component */
   for (j = 0; j < nX; j++){          // y
@@ -245,24 +203,13 @@ void AT_GSM_dose_grid_from_particles_positions( const long  number_of_field_comp
       } // field components
     } // x loop
   } // y loop
-  
-  end = time(NULL);
-  printf("step9 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
 
   for (i = 0; i < number_of_field_components; i++){
     free(doses[i]);
   }
-
-  end = time(NULL);
-  printf("step10 time %f [s]\n", difftime(end, begin));
-  begin = time(NULL);
-
   free(doses);
   free(distances_index);
   free(r_max_m);
-  printf("stop AT_GSM_dose_grid_from_particles_positions\n");
-
 }
 
 
@@ -274,10 +221,6 @@ void AT_GSM_local_dose_distrib_from_dose_grid( const long     nX,
     double         frequency[]){
 
   long i, j, bin_no;
-
-  printf("start AT_GSM_local_dose_distrib_from_dose_grid\n");
-
-  printf("step 1\n");
 
   // Fill dose into histogram
   /* On histograms and binning in libamtrack, see documentation */
@@ -298,15 +241,10 @@ void AT_GSM_local_dose_distrib_from_dose_grid( const long     nX,
       }
     }
   }
-
-  printf("step 2\n");
   for(i = 0; i < number_of_bins; i++){
     frequency[i]   /= gsl_pow_2(nX);
   }
-
-  printf("step 3\n");
   (*zero_fraction) /= gsl_pow_2(nX);
-  printf("step 4\n");
 }
 
 
@@ -645,31 +583,6 @@ void AT_GSM_multiple_local_dose_distrib( const long  number_of_field_components,
 
 	long i,j,k;
 
-  // print all input arguments
-  printf("number_of_field_components = %ld\n", number_of_field_components);
-  printf("E_MeV_u = %g\n", E_MeV_u[0]);
-  printf("fluence_cm2 = %g\n", fluence_cm2[0]);
-  printf("particle_no = %ld\n", particle_no[0]);
-  printf("material_no = %ld\n", material_no);
-  printf("rdd_model = %ld\n", rdd_model);
-  printf("rdd_parameter = %g\n", rdd_parameter[0]);
-  printf("er_model = %ld\n", er_model);
-  printf("stopping_power_source_no = %ld\n", stopping_power_source_no);
-  printf("nX = %ld\n", nX);
-  printf("pixel_size_m = %g\n", pixel_size_m);
-  printf("N_runs = %ld\n", N_runs);
-  printf("N_repetitions = %ld\n", N_repetitions);
-  printf("number_of_bins = %ld\n", number_of_bins);
-  printf("dose_bin_centers_Gy = %g\n", dose_bin_centers_Gy[0]);
-  printf("dose_bin_width_Gy = %g\n", dose_bin_width_Gy[0]);
-  printf("mean_d_check_Gy = %g\n", *mean_d_check_Gy);
-  printf("sd_d_check_Gy = %g\n", *sd_d_check_Gy);
-  printf("mean_zero_dose_fraction = %g\n", *mean_zero_dose_fraction);
-  printf("sd_zero_dose_fraction = %g\n", *sd_zero_dose_fraction);
-  printf("mean_dose_frequency_Gy = %g\n", mean_dose_frequency_Gy[0]);
-  printf("sd_dose_frequency_Gy = %g\n", sd_dose_frequency_Gy[0]);
-
-
 	AT_histoOld_get_bin_widths(	number_of_bins,
 								dose_bin_centers_Gy,
 								dose_bin_width_Gy);
@@ -781,41 +694,14 @@ void AT_GSM_local_dose_distrib( const long  number_of_field_components,
 
   long    i;
 
-  // print all input parameters
-  printf("INPUT: number_of_field_components = %ld\n", number_of_field_components);
-  printf("INPUT: E_MeV_u = %g\n", E_MeV_u[0]);
-  printf("INPUT: fluence_cm2 = %g\n", fluence_cm2[0]);
-  printf("INPUT: particle_no = %ld\n", particle_no[0]);
-  printf("INPUT: material_no = %ld\n", material_no);
-  printf("INPUT: rdd_model = %ld\n", rdd_model);
-  printf("INPUT: rdd_parameter = %g\n", rdd_parameter[0]);
-  printf("INPUT: er_model = %ld\n", er_model);
-  printf("INPUT: stopping_power_source_no = %ld\n", stopping_power_source_no);
-  printf("INPUT: nX = %ld\n", nX);
-  printf("INPUT: pixel_size_m = %g\n", pixel_size_m);
-  printf("INPUT: number_of_bins = %ld\n", number_of_bins);
-  printf("INPUT: dose_bin_centers_Gy[0] = %g\n", dose_bin_centers_Gy[0]);
-  printf("INPUT: dose_bin_centers_Gy[number_of_bins-1] = %g\n", dose_bin_centers_Gy[number_of_bins-1]);
-  printf("INPUT: random_number_generator_seed = %ld\n", *random_number_generator_seed);
-  printf("INPUT: zero_dose_fraction = %g\n", *zero_dose_fraction);
-  printf("INPUT: dose_frequency_Gy = %g\n", dose_frequency_Gy[0]);
-
-
-  
-  printf("AT_GSM_local_dose_distrib\n");
-  printf("input: number_of_field_components: %ld \n", number_of_field_components);
-
   /* find maximum of maximal delta-electron ranges */
   double max_r_max_m = 0.0;
   for (i = 0; i < number_of_field_components; i++){
     max_r_max_m    =   GSL_MAX(max_r_max_m, AT_max_electron_range_m(E_MeV_u[i], material_no, er_model));
-    printf("Field %ld / %ld,  Rmax = %g [m]\n", i, number_of_field_components, max_r_max_m);
   }
-  printf("info: max_r_max_m: %g \n", max_r_max_m);
 
   /* largest r.max --> calculate size of sample area */
   double sample_grid_size_m    = pixel_size_m * nX + 2.01 * max_r_max_m;
-  printf("info: sample_grid_size_m: %g \n", sample_grid_size_m);
 
   long*  number_of_particles_in_field_component   =  (long*)calloc(number_of_field_components, sizeof(double));
   double** x_position = (double**)calloc(number_of_field_components, sizeof(double*));
@@ -837,9 +723,6 @@ void AT_GSM_local_dose_distrib( const long  number_of_field_components,
                   x_position,
                   y_position);
 
-  printf("x_position: %p \n", x_position);
-  printf("x_position[0]: %p \n", x_position[0]);
-
   /* calculate dose deposition pattern in grid cells */
   AT_GSM_dose_grid_from_particles_positions( number_of_field_components,
       E_MeV_u,
@@ -856,10 +739,6 @@ void AT_GSM_local_dose_distrib( const long  number_of_field_components,
       pixel_size_m,
       grid_D_Gy);
 
-  printf("x_position: %p \n", x_position);
-  printf("x_position[0]: %p \n", x_position[0]);
-
-
   /* calculate dose frequency from dose pattern */
   AT_GSM_local_dose_distrib_from_dose_grid( nX,
       (const double**)grid_D_Gy,
@@ -868,37 +747,19 @@ void AT_GSM_local_dose_distrib( const long  number_of_field_components,
       zero_dose_fraction,
       dose_frequency_Gy);
 
-  printf("x_position: %p \n", x_position);
-  printf("x_position[0]: %p \n", x_position[0]);
-  printf("Free 1\n");
-
   /* free memory */
   for (i = 0; i < number_of_field_components; i++){
-    printf("i = %ld \n", i);
-    printf("freeing x_position \n");
-    printf("x_position[i] = %p \n", x_position[i]);
-    printf("x_position[i][0] = %g \n", x_position[i][0]);
     free( x_position[i] );
-    printf("freeing y_position \n");
     free( y_position[i] );
   }
 
-  printf("x_position: %p \n", x_position);
-  printf("x_position[0]: %p \n", x_position[0]);
-
-  printf("Free 2\n");
   free( grid_D_Gy[0] );
-
-  printf("Free 3\n");
   free( grid_D_Gy );
 
-  printf("Free 4\n");
   free( number_of_particles_in_field_component );
-  printf("Free 5\n");
-  printf("x_position: %p \n", x_position);
+
   free( x_position );
-  printf("Free 6\n");
   free( y_position );
-  printf("Free 7\n");
+
 }
 
