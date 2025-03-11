d1adccbb7e57f475ed75751d35b58c763d0eba39
Niels Bassler <bassler@clin.au.dk>
2025-03-11 19:08:06 +0100

    fix #247 add versioning

---
+++ include/AT_version.h
@@ -0,0 +1,8 @@
+/* dummy file which will be overwritten by cmake, but makes IntelliSense happy */
+#ifndef AT_VERSION_H
+#define AT_VERSION_H
+
+#define AT_VERSION "0.0.0"
+#define AT_VERSION_GIT "0.0.0+0.gfffffff"
+
+#endif // AT_VERSION_H
